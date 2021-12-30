344d9f471415b28c40a24d95599c0af960e64ea3
pranav <pranav8iisc@gmail.com>
2021-12-30 11:44:42 +0530

    Update lru-cache.py

--- src/algorithms/lru-cache.py
+++ src/algorithms/lru-cache.py
@@ -34,7 +34,6 @@ class LRUCache:
         the capacity of the cache should be intitialized.
         '''
         self.cap = capacity
-        #print("Cap: ", self.cap)
         self.current_cache_size = 0
         self.hMap = {}
         self.cache_head = None
@@ -60,24 +59,17 @@ class LRUCache:
         if self.hMap and key in self.hMap.keys():            
             frame_node = self.hMap[key]
             if frame_node is not self.cache_head:
-#                print("4: ? :" ,frame_node.prev.data)
                 frame_node.prev.next = frame_node.next                
                 if frame_node is self.cache_tail:
                     self.cache_tail = frame_node.prev
                 else:
                     frame_node.next.prev = frame_node.prev
-                    #print("YESY check")
-                    #self.printCache()
                 frame_node.prev = None
                 frame_node.next = self.cache_head
-                #print("Check")
-                #self.printCache()
                 self.cache_head.prev = frame_node
                 self.cache_head = frame_node
-            #self.printCache()
             return frame_node.data
         else:
-            #self.printCache()
             return -1        
 
     def put(self, key: int, value: int) -> None:
@@ -97,13 +89,13 @@ class LRUCache:
         elif key in self.hMap.keys(): # key is in cache
             frame_node = self.hMap[key]
             frame_node.data = value
-            #self.update_cache_head(frame_node, key) # only purpose is to bring frame_node to cache_head
             if frame_node is self.cache_head: 
-    #            self.printCache()
                 return 
             frame_node.prev.next = frame_node.next
             if frame_node is self.cache_tail:
                 self.cache_tail = frame_node.prev
+            else:
+                frame_node.next.prev = frame_node.prev
             frame_node.prev = None
             frame_node.next = self.cache_head
             self.cache_head.prev = frame_node
@@ -119,18 +111,20 @@ class LRUCache:
             else:
                 # evict LRU
                 prev_tail_node = self.cache_tail
-                if self.cache_tail is not self.cache_head:
+                if self.cache_tail is not self.cache_head:                    
                     self.cache_tail = self.cache_tail.prev
                     self.cache_tail.next = None
-                # https://stackoverflow.com/a/13149770/985166
+                    frame_node.next = self.cache_head
+                    self.cache_head.prev = frame_node
+                else:
+                    self.cache_tail = frame_node  
+                # https://stackoverflow.com/a/13149770/985166                
                 evicted_node_key = list(self.hMap.keys())[list(self.hMap.values()).index(prev_tail_node)]
                 self.hMap.pop(evicted_node_key) # remove reference to evicted node
                 del prev_tail_node # remove evicted node from cache
                 self.hMap[key] = frame_node
-                frame_node.next = self.cache_head
-                self.cache_head.prev = frame_node
                 self.cache_head = frame_node
-     #   self.printCache()                   
+                
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
