d44605c5234e51baf5a3926cee7e1484b13e93f2
pranav <pranav8iisc@gmail.com>
2022-01-13 19:27:31 +0530

    WIP: added dfs implementation

--- src/algorithms/num-of-islands.py
+++ src/algorithms/num-of-islands.py
@@ -1,9 +1,36 @@
 class Solution:
-    def numIslandsDFS(self, grid: List[List[str]]) -> int:
+    def dfs(self, row: int, col: int, grid: List[List[str]])-> None:
+        grid[row][col] = '#'
+        if row - 1 > 0 and grid[row - 1][col] == '1':
+            self.dfs(row - 1, col, grid)
+        if row + 1 < len(grid) and grid[row + 1][col] == '1':
+            self.dfs(row + 1, col, grid)
+        if col - 1 > 0 and grid[row][col - 1] == '1':
+            self.dfs(row, col - 1, grid)
+        if col + 1 < len(grid[0]) and grid[row][col + 1] == '1':
+            self.dfs(row, col + 1, grid)
+        return
+        
+    def numIslands(self, grid: List[List[str]]) -> int:
         '''
-        For each 1 in grid, do DFS from that position, during DFS mark eqach visited
-        cell as # and increment island counter
+        if an element on the grid is 1, do DFS from there, mark each 1 element
+        visited on the island as #/0 etc so as to avoid visiting starting DFS from there
+        increment the counter by 1
+        return counter
         '''
+        island_counter = 0
+        n_rows = len(grid)
+        n_cols = len(grid[0])
+        for row in range(n_rows):
+            for col in range(n_cols):
+                if grid[row][col] == '1':
+                    self.dfs(row, col, grid)
+                    island_counter += 1
+                else:
+                    continue
+                    
+        return island_counter
+    
     def numIslandsNotWorking(self, grid: List[List[str]]) -> int:        
         num_islands = 0
         #island_ids = []
@@ -26,6 +53,8 @@ class Solution:
                         #        island_started = False                                
                 else:
                     continue
-        return num_islands                      
+        return num_islands               
+    
+    
         
         
