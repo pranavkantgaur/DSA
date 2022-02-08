46bcdb43b291285ee2b8cb85e9eb56a0d0cc8004
pranav <pranav8iisc@gmail.com>
2022-02-08 10:55:37 +0530

    Update remove-nth-node-from-end-of-list.py

--- src/algorithms/remove-nth-node-from-end-of-list.py
+++ src/algorithms/remove-nth-node-from-end-of-list.py
@@ -4,14 +4,22 @@
 #         self.val = val
 #         self.next = next
 '''
-2-pass approach:
+# edge cases:
+1. n = length
+2. n = 1
+3. length = 1
+
+
+# 2-pass approach:
 1. Compute length of list by iterating on all elements
 2. If length > n:
    2.1. Reach n+1 element from last and remove nth node:
         node_to_be_deleted = node.next
         node.next = node_to_be_deleted.next
         delete node_to_be_deleted
-1-pass approach:
+
+
+# 1-pass approach:
 1. Maintain a slow pointer, fast pointer
 2. Difference in position of slow and fast pointerw will be n
 3. Once fast pointer reaches the end, delete slow.next node
@@ -24,11 +32,18 @@ class Solution:
         while node.next is not None:
             node = node.next
             length += 1
+        # check for edge-cases
+        if length == n:
+            node_to_be_deleted = head
+            head = head.next
+            del node_to_be_deleted
+            return head        
         # locate predecessor of the node to be deleted
         current_node_id = 1
         node = head
         while(current_node_id < length - n):
             node = node.next
+            current_node_id += 1
         # delete the node
         node_to_be_deleted = node.next
         node.next = node_to_be_deleted.next
