6850888e833e1ab6acd5e0baaa6a3dda7dfdfb10
pranav <pranav8iisc@gmail.com>
2022-02-10 11:44:27 +0530

    Update merge-2-sorted-lists.py

--- src/algorithms/merge-2-sorted-lists.py
+++ src/algorithms/merge-2-sorted-lists.py
@@ -3,6 +3,30 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+'''
+https://leetcode.com/problems/merge-two-sorted-lists/discuss/9735/Python-solutions-(iteratively-recursively-iteratively-in-place).
+1. Iterative
+   1. Start with first node in list1:
+      1.1. check if it is smaller than first node in list2, 
+      1.2. if not add node2 in new list till the value of node in list2 is less than or equal to node 1
+      1.3. add node1 to the result list
+   2. if there are nodes in list2 add them to result list   
+   
+2. Recursive
+3. Iterative, in-place:
+   * Given 2 sorted lists, merge them in-place: Inplace-merging means creating merged linked-list without using additional space (i.e., SC: O(1)).
+   * Maintain 3 head pointers, 1 each for part of list1, list2 remaining to be processed and 1 for result list
+   * start with list1: check if first iter of list1 is smaller than that of list2, if so, make list1's first node as head of result list: resultHead = list[0] 
+     and advance list1head (thwereby shrinking list1 and expanding resultList)
+     * if node in list2 is smaller than node in list1 then resultHead = list2[0]
+       * for all subsequent nodes in list2 with value less than that of the node pointed to by head of list1: continue traversing list2 and advancing list2head
+         (advancing list2 head has the effect of list2 getting shrunked and resultlist expanding)    
+   * At the end of loop, when either list1 or list2 is empty, return head      
+'''
+
+
+
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         if list1 is None and list2 is not None:
