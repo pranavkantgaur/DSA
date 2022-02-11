db17d94137fdcd03a81c42ba4ac595855a149601
pranav <pranav8iisc@gmail.com>
2022-02-11 11:35:43 +0530

    Update merge-2-sorted-lists.py

--- src/algorithms/merge-2-sorted-lists.py
+++ src/algorithms/merge-2-sorted-lists.py
@@ -17,17 +17,52 @@ https://leetcode.com/problems/merge-two-sorted-lists/discuss/9735/Python-solutio
 3. Iterative, in-place:
    * Given 2 sorted lists, merge them in-place: Inplace-merging means creating merged linked-list without using additional space (i.e., SC: O(1)).
    * Maintain 3 head pointers, 1 each for part of list1, list2 remaining to be processed and 1 for result list
-   * start with list1: check if first iter of list1 is smaller than that of list2, if so, make list1's first node as head of result list: resultHead = list[0] 
-     and advance list1head (thwereby shrinking list1 and expanding resultList)
+   * start with list1: check if first item of list1 is smaller than that of list2, if so, make list1's first node as head of result list: resultHead = list1[0] 
+     and advance list1head (thereby shrinking list1 and expanding resultList)
      * if node in list2 is smaller than node in list1 then resultHead = list2[0]
        * for all subsequent nodes in list2 with value less than that of the node pointed to by head of list1: continue traversing list2 and advancing list2head
          (advancing list2 head has the effect of list2 getting shrunked and resultlist expanding)    
-   * At the end of loop, when either list1 or list2 is empty, return head      
+   * At the end of loop, when either list1 or list2 is empty, return head   
+'''
+      
+'''
+Input: list1 = [1,2,4], list2 = [1,3,4]
+Output: [1,1,2,3,4,4]
+
+Input: list1 = [], list2 = []
+Output: []
+
+Input: list1 = [], list2 = [0]
+Output: [0]
 '''
 
 
 
 class Solution:
+    def mergeTwoListsInPlace(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+         list1head = list1[0]
+         list2head = list2[0]
+         resulthead = None
+         resulttail = None
+         while list1head or list2head:
+            if list1head.val < list2head.val:
+               if not resulthead:
+                  resulthead = list1head         
+                  resulttail = list1head
+               else:
+                  resulttail.next = list1head
+                  resulttail = list1head
+                  list1head = list1head.next
+            else:
+               if not resulthead:
+                  resulthead = list2head         
+                  resulttail = list2head
+               else:
+                  resulttail.next = list2head
+                  resulttail = list2head
+                  list2head = list2head.next            
+         return resulthead
+         
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         if list1 is None and list2 is not None:
             return list2
