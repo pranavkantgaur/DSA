62cf7f440c974381fc40d0230547e2a31148fbdd
pranav <pranav8iisc@gmail.com>
2022-02-21 18:37:55 +0530

    Update remove-nth-node-from-end-of-list.py

--- src/algorithms/remove-nth-node-from-end-of-list.py
+++ src/algorithms/remove-nth-node-from-end-of-list.py
@@ -4,6 +4,7 @@
 #         self.val = val
 #         self.next = next
 class Solution:
+    # todo: CHECK FOR head = [1,2], n = 2
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
         if n == 1 and not head.next:
             return None
@@ -13,10 +14,13 @@ class Solution:
         while(count > 0):
             fast = fast.next
             count -= 1
-        while(fast and fast.next):
+        if not fast: # means we want to delete the head node (Does it always hold?)
+            head = head.next
+            return head
+         while(fast.next):
             slow = slow.next
             fast = fast.next
-        
+            
         node_to_be_deleted = slow.next
         slow.next = slow.next.next     
         return head
