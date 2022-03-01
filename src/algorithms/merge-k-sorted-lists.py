fee71442aa323d54065c3b07300da6f6a1a54113
pranav <pranav8iisc@gmail.com>
2022-03-01 13:35:26 +0530

    Update merge-k-sorted-lists.py

--- src/algorithms/merge-k-sorted-lists.py
+++ src/algorithms/merge-k-sorted-lists.py
@@ -3,33 +3,46 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-'''
-Approach - 1: Merge 2 lists at a time for k - 1 times
-
-'''
 class Solution:
-    def merge2Lists(self, merged_list, next_list):
-        result_list = []
-        node1 = merged_list[0]
-        node2 = next_list[0]
-        while node1 is not None:            
-            if node1.val >= node2.val:
-                result_list.append(node2)
-                if result_list:
-                    result_list[-1].next = node2
-            if result_list:
-                result_list[-1].next = node1
-            result_list.append(node1)   
-            result_list[-1].next = None
-            node1 = node1.next
-        return result_list          
-            
-                
-                
-        
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        list1head = list1
+        list2head = list2
+        resulthead = None
+        resulttail = None
+        while list1head and list2head:
+            if list1head.val < list2head.val:
+                if not resulthead:
+                    resulthead = list1head         
+                    resulttail = list1head
+                    
+                else:
+                    resulttail.next = list1head
+                    resulttail = list1head
+                list1head = list1head.next
+            else:
+                if not resulthead:
+                    resulthead = list2head         
+                    resulttail = list2head
+                else:
+                    resulttail.next = list2head
+                    resulttail = list2head
+                list2head = list2head.next            
+        if list1head:
+            if resulttail:
+                resulttail.next = list1head
+            else:
+                resulthead = list1head
+        if list2head:
+            if resulttail:
+                resulttail.next = list2head
+            else:
+                resulthead = list2head
+        return resulthead
+
+    
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        merged_list = list[0]
-        for next_list in range(lists[1:]):
-            merged_list = self.merge2Lists(merged_list, next_list)
-        return merged_list    
+        merged_list = lists[0]
+        for next_list in lists[1:]:
+            merged_list = self.mergeTwoLists(merged_list, next_list)
+        return merged_list  
         
