8505a7ef659fc6020a3b70d8a197e67deaf1f5ed
pranav <pranav8iisc@gmail.com>
2022-04-18 15:16:17 +0530

    Update check-balanced-binary-tree.py

--- src/algorithms/check-balanced-binary-tree.py
+++ src/algorithms/check-balanced-binary-tree.py
@@ -8,19 +8,20 @@
 
 
 class Solution:
-
     def heightOfTree(self, root):
         if not root:
             return -1
         return 1 + max(self.heightOfTree(root.left), self.heightOfTree(root.right))
     
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        if root:
-            if self.isBalanced(root.left) and self.isBalanced(root.right):
+        if root: # 0
+            if self.isBalanced(root.left) and self.isBalanced(root.right): # 4 4
                 if abs(self.heightOfTree(root.left) - self.heightOfTree(root.right)) <= 1 :
                     return True
                 else:
                     return False
+            else:
+                return False
         else:
             return True
-                
+                        
