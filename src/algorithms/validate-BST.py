fcbd7ef8e309031ff3810d95d283ce2302f22a96
pranav <pranav8iisc@gmail.com>
2022-04-18 15:45:19 +0530

    Update validate-BST.py

--- src/algorithms/validate-BST.py
+++ src/algorithms/validate-BST.py
@@ -8,15 +8,43 @@
 class Solution:
     
     def getMaxValueBST(self, bst_root):
-        pass
+        '''
+        return the value of the rightmost node
+        '''
+        node = bst_root
+        while node.right:
+            node = node.right
+        return node.val                        
+        
     def getMinValueBST(self, bst_root):
-        pass
+        '''
+        return value of the leftmost node
+        '''
+        node = bst_root
+        while node.left:
+            node  = node.left
+        return node.val            
     
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        if not root:
+        if root.left is None and root.right is None: # left node
             return True
-        if self.isValidBST(root.left) and self.isValidBST(root.right) 
-            if self.getMaxValueBST(root.left) < root.key and self.getMinValueBST(root.right) > root.key:
+        '''
+        if root.left is null and root.right is not null, check if root.right.key > root.key: true else false
+        if root.left is not null and root.right is null, check if root.left.key < root.key: true else false
+        '''
+        if root.left is None and root.right:
+            if root.right.val > root.val: 
+                return True 
+            else:
+                return False
+        if root.left and root.right is None:
+            if root.left.val < root.val: 
+                return True
+            else:
+                return False
+        # in other cases            
+        if self.isValidBST(root.left) and self.isValidBST(root.right): 
+            if self.getMaxValueBST(root.left) < root.val and self.getMinValueBST(root.right) > root.val:
                 return True
             else:
                 return False
