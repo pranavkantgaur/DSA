5ad911d999b1efd824c18abc39bb276ae46770e0
pranav <pranav8iisc@gmail.com>
2022-04-19 18:15:58 +0530

    Update find-first-last-pos.py

--- src/algorithms/find-first-last-pos.py
+++ src/algorithms/find-first-last-pos.py
@@ -1,21 +1,100 @@
 '''
 https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+
 BF: 
 0. Check if arr[0] <= target <= arr[n - 1]: if true then continue else return [-1, -1]
 1. Linearly search for the element from left to right
-2. Mark the first position where the element is found
-3. Continue untill current element is different from target value, set last index as current - 1
+2. Mark the first position where the element is found as start
+3. Continue untill current element is different from target value, set last index as current - 1, mark as last
 4. return start, last index
 
 TC: O(n)
 SC: O(1)
 
 Binary search:
-0. Check if arr[0] <= target <= arr[n - 1]: if true then continue else return [-1, -1]
-1. Locate the element in the array, say index i = bsearch(low, high, target)
-2. Check if a[i - 1] == target, if true: bsearch(low, i - 1, target) 
-3. Check if a[i + 1] == target, if true: bsearch(i + 1, high, target)
+-1. if arr is None: return [-1, -1]
+0. Check if arr[0] <= target <= arr[n - 1]: if true then continue else return [-1, -1] # sanity check
+1. Locate the element in the array, say index i = bsearch(low, high, target) else return [-1, -1] # within range but not in array
+2. Check if a[i - 1] == target, if true: k = bsearch(low, i - 1, target), else, set start = i # fix start with binary searches
+   2.-1. if i == 0: start = i, and skip 2.x
+   2.0. k = i
+   2.1. while(a[k - 1] == target):
+          high = k - 1
+          if high == 0:
+            start = 0
+            break
+          else:
+            k = bsearch(low, high, target)
+   2.2. start = k       
+3. Check if a[i + 1] == target, if true: m = bsearch(i + 1, high, target), end = m, else, set end = i # fix end with binary searches
+   3.-1. if i == len(a) - 1: end = i, skip 3.x
+   3.0. k = i
+   3.1. while(a[k + 1] == target):
+          low = k + 1
+          if low == len(a) - 1:
+            end = low
+            break
+          else:  
+            k = bsearch(low, high, target)
+   3.2. end = k       
+4. return [start , end]
 '''
+
 class Solution:
+    def bsearch(self, low, high, target, nums):
+        while(low <= high):
+            mid = low + (high - low) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                high = mid - 1
+            else:
+                low = mid + 1
+        return -1    
+            
+        
     def searchRange(self, nums: List[int], target: int) -> List[int]:
+        if nums is None or nums[0] > target or nums[-1] < target:  # sanity checks
+            return [-1, -1]
+        low = 0
+        high = len(nums) - 1
+        i = self.bsearch(low, high, target, nums)
+        if i == -1: # sanity check
+            return [-1, -1]
+        else:
+            start = -1
+            end = -1
+            # locate the start
+            if i == 0:
+                start = i
+            else:
+                k = i
+                while(nums[k - 1] == target):
+                    high = k - 1
+                    if high == 0:
+                        #start = high
+                        break
+                    k = self.bsearch(low, high, target, nums)                        
+                    #start = k # TODO, make this outside while loop   
+                if k == 0:
+                    start = 0
+                else:
+                    start = k
+            # locate the end
+            if i == len(nums) - 1:
+                end = i
+            else:
+                k = i
+                while(nums[k + 1] == target):
+                    low = k + 1
+                    if low == len(nums) - 1:
+                        #end = low                        
+                        break
+                    k = self.bsearch(low, high, target, nums)   
+                if k == len(nums) - 1:
+                    end = low
+                else:
+                    end = k  # TODO, make this outside while loop  
+            
+            return [start, end]
         
