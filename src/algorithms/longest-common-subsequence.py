a7c857913d9f1b206056f5fb1714d82f76d16a6f
pranav <pranav8iisc@gmail.com>
2022-04-21 11:34:30 +0530

    Update longest-common-subsequence.py

--- src/algorithms/longest-common-subsequence.py
+++ src/algorithms/longest-common-subsequence.py
@@ -1,3 +1,4 @@
+# https://leetcode.com/problems/longest-common-subsequence/
 '''
 BF:
 0. Set lcs_len = 0
@@ -17,9 +18,12 @@ SC: O(1)
 
 
 Iterative string matching:
-1. For each char in s2 mark all positions where it occurs in s1, if none then increment s2 char pointer
-2. if char is found a n positon in s1: parallely check remaining letters of s2 in following substring in s1(wrt all position marked in step-1)
-3. TODO
+0. current_substrings = [s1], substrings_with_l = [], lcs = []
+1. For each letter l in s2:
+   1.0. for all substrings in current_substrings, which contain l:
+        1.1. update current_substrings = getNewSubstrings(l, current_substrings)
+        1.2. Append l to current lcs
+2. Return length of lcs
 
 
 Optimization:
@@ -44,9 +48,40 @@ Output: 3
 Input: text1 = "abcde", text2 = "cer" # ce is lcs
 Output: 2 
 
-More TODO
+
+
+More TODO, test cases with multiple common and disjoint(not like subsets of each other) subsequences
 
 '''
 class Solution:
+    def getIndexOfLetter(self, letter, substring):
+        # https://stackoverflow.com/a/11122355
+        return [i for i, ltr in enumerate(substring) if ltr == letter]
+        
+    def getNewSubstrings(self, letter, current_substrings):
+        new_substrings = []
+        for substring in current_substrings:            
+            print('In substring: ', substring)
+            if letter in substring:
+                print('Letter:', letter)
+                letter_indices = self.getIndexOfLetter(letter, substring)
+                print('Found at: ', letter_indices)
+                for index in letter_indices:
+                    new_substrings.append(substring[index:])
+            else:
+                print('No letter: ', letter)
+        return new_substrings                
+        
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        current_substrings = [text1]
+        lcs = []
+        for letter in text2:
+            matching_substrings = self.getMatchingSubstrings(letter, current_substrings) # all substrings containing letter 
+            for substring in matching_substrings: # iterate over matching substrings
+                #print("Old substrings: ", current_substrings)
+                current_substrings = self.getNewSubstrings(letter, current_substrings) # get substrings of matching substrings where to continue searching of upcoming letters from text2
+                #print("New substrings: ", current_substrings)
+                lcs.append(letter)
+        print("LCS: ", lcs)
+        return len(lcs)                
         
