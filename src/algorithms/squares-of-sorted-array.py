8bc3bd7e7e79a6eaf677c096544e38705cbc90f7
pranav <pranav8iisc@gmail.com>
2022-04-29 16:41:39 +0530

    Update squares-of-sorted-array.py

--- src/algorithms/squares-of-sorted-array.py
+++ src/algorithms/squares-of-sorted-array.py
@@ -1,5 +1,14 @@
 # https://leetcode.com/problems/squares-of-a-sorted-array/
 
+'''
+Approaches:
+0. Square all elements, return the sorted result.
+1. Check if all elements positive then simply square and return, if not binary search for abs(negative) in test of the array and place elements
+2. Keep left and right pointer and place the bigger of the two in result array, regardless of the sign.
+3. Inplace and O(n) solution: Can we optimize approach-2 for inplace? Best solutions(in discuss section) are also not inplace.
+   
+'''
+
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
         '''
@@ -26,7 +35,8 @@ class Solution:
                     result.append(pow(nums[end], 2))
                     start += 1
                     end -= 1
-            result.append(pow(nums[start], 2)) # for start = end case       
+            if start == end:
+                result.append(pow(nums[start], 2))      
             #print('resutlt: ', result)        
         return reversed(result)
         
