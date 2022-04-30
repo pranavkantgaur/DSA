501f5acc5b7188665913f1b07f6ce937a06dbe7f
pranav <pranav8iisc@gmail.com>
2022-04-30 06:46:06 +0530

    Update max-sum-subarray.py

--- src/algorithms/max-sum-subarray.py
+++ src/algorithms/max-sum-subarray.py
@@ -1,1 +1,10 @@
 # https://leetcode.com/problems/maximum-subarray/
+
+'''
+ Set start=0, end=k-1
+ Compute sum of subarray (start, end)
+ Update max sum
+ While (end < len(array)): if end is not last element: sum -= arr(start) + arr(end)
+ Check in while loop if sum is greater than the max, if yes update else continue
+ At the end of while loop, return max sum
+'''
