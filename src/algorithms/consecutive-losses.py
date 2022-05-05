5dbd4723ccf5526441fb6c6572018dac7b704958
pranav <pranav8iisc@gmail.com>
2022-05-05 17:20:38 +0530

    Create consecutive-losses.py

---
+++ src/algorithms/consecutive-losses.py
@@ -0,0 +1,8 @@
+# https://www.interviewbit.com/problems/consecutive-losses/
+'''
+A string of length A is like: WLWWLW...A times
+Find string combinations with 'L' repeated consecutively a maximum of K times.
+* Sliding window of size K?
+
+
+'''
