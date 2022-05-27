11a89369514964a408b447d8fde80f5b86f882a3
pranav <pranav8iisc@gmail.com>
2022-05-27 10:47:01 +0530

    Update combination-sum.py

--- src/algorithms/combination-sum.py
+++ src/algorithms/combination-sum.py
@@ -1,21 +1,16 @@
 # https://leetcode.com/problems/combination-sum/
-from collections import Counter
-
 class Solution:      
 
     def backtrack(self, state, candidates, target, result):
              if target == 0:
                 temp_state = state.copy()
-                for a_state in result: # to avoid duplicates
-                    if Counter(a_state) == Counter(state):
-                        return
                 result.append(temp_state)
                 return
              else:
-                for candidate in candidates:
+                for index, candidate in enumerate(candidates):
                     if target - candidate >= 0:
                         state.append(candidate)
-                        self.backtrack(state, candidates, target - candidate, result)
+                        self.backtrack(state, candidates[index:], target - candidate, result)
                         explored_value = state.pop()
                     else:
                         continue
@@ -28,5 +23,7 @@ class Solution:
         self.backtrack(state, candidates, target, result)
         return result
         
+
+                
 
         
