77a0409827d8ee26e3b6d30a135bb27642bc5810
pranav <pranav8iisc@gmail.com>
2022-06-03 11:03:42 +0530

    Update max-sum-subarray.py

--- src/algorithms/max-sum-subarray.py
+++ src/algorithms/max-sum-subarray.py
@@ -1,10 +1,40 @@
 # https://leetcode.com/problems/maximum-subarray/
+'''
+Iterative:
+1. if all elements of the array are +ve, return sum of array elements
+2. If all elements are -ve, return the min element
+3. Create islands of =ve nuymbers, merge +ve subarray as a single number:
+   --- + ---- + -- + (+ is a single number after merging +ve numbers, - is a -ve number)
+4. Merging +----+ if sum is greater than max(+, +)  
 
+Recursive:
+1. return max(num + maxSubArray(nums - num), maxSubArray())
 '''
- Set start=0, end=k-1
- Compute sum of subarray (start, end)
- Update max sum
- While (end < len(array)): if end is not last element: sum -= arr(start) + arr(end)
- Check in while loop if sum is greater than the max, if yes update else continue
- At the end of while loop, return max sum
-'''
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        return sum(nums) if all num in nums > 0
+        return min(nums) if all num in nums < 0
+        for index, num in enumerate(nums):
+            if num > 0:
+                start = index
+            if num < 0:
+                end = index
+                sum_element = sum(nums[start:end])
+                nums.remove(start:end)                
+                nums.add(start, sum_element)
+        for num in nums:
+            if num < 0:
+                continue
+            else:
+                if start is not None:
+                    end = index    
+                    if sum(nums[start:end] > max(nums[start], nums[end])):
+                        sum_element = sum(nums[start:end])
+                        nums.remove(start:end)
+                        nums.add(start, sum_element)
+                    start = index                        
+                else:
+                    start = index
+                    
+                
+        
