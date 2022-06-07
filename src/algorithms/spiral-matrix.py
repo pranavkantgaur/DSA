e7e400caf445986e3459e4ba4a473ec1e346b3e0
pranav <pranav8iisc@gmail.com>
2022-06-07 18:17:33 +0530

    Update spiral-matrix.py

--- src/algorithms/spiral-matrix.py
+++ src/algorithms/spiral-matrix.py
@@ -1,3 +1,4 @@
+# https://leetcode.com/problems/spiral-matrix/
 '''
 1. Set TL = [0,0], TR=[0, m-1], BL = [n - 1, 0], BR=[n-1, m-1]
 2. For i from 1 to X:
@@ -10,4 +11,14 @@
 '''
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        TL = [0, 0]
+        TR = [0, len(matrix) - 1]
+        BL = [len(matrix[0]) - 1, 0]
+        BR = [len(matrix[0]) - 1, len(matrix) - 1]
+        for i in range(N):
+            seed = [TL[0] + i, TL[1] + i]            
+            # 2.1. Print from TL + seed_x to TR - seed_x 
+            # 2.2. Print from TR + seed_y  to BR - seed_y 
+            # 2.3. Print from BR - seed_x to BL - seed_x 
+            # 2.4. Print from BL - seed_Y to TR - seed_Y 
         
