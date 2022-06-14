f6fc7ac6f7e2aca064be8eef7d343171becf96aa
pranav <pranav8iisc@gmail.com>
2022-06-14 12:12:09 +0530

    Update spiral-matrix.py

--- src/algorithms/spiral-matrix.py
+++ src/algorithms/spiral-matrix.py
@@ -11,41 +11,24 @@
 '''
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        rows = len(matrix)
-        cols = len(matrix[0])
-        TL = [0, 0]
-        TR = [0, cols - 1]
-        BL = [rows - 1, 0]
-        BR = [rows - 1, cols - 1]
+        rowStart = 0
+        rowEnd = len(matrix) - 1
+        colStart = 0
+        colEnd = len(matrix[0]) - 1
         result = []
-        N = rows - 2 # TODO, fix
-        for i in range(N):
-            # 2.1. Print from TL + seed_x to TR - seed_x 
-            a = TL[0] + i
-            b = TR[0] - i
-            row = i
-            for j in range(a, b):
-                result.append(matrix[row][j])
-                
-            # 2.2. Print from TR + seed_y  to BR - seed_y 
-            a = TR[1] + i
-            b = BR[1] - i
-            col = len(matrix[0]) - 1 - i
-            for j in range(a, b):
-                result.append(matrix[j][col])
-
-            # 2.3. Print from BR - seed_x to BL - seed_x 
-            a = BR[0] - i
-            b = BL[0] + i
-            row = len(matrix) - 1 - i
-            for j in range(a, b):
-                result.append(matrix[row][j])
-
-            # 2.4. Print from BL - seed_Y to TR - seed_Y 
-            a = BL[1] - i
-            b = TL[1] + i
-            col = i
-            for j in range(a, b):
-                result.append(matrix[j][col])
-                
-        return result        
+        while(rowStart <= rowEnd and colStart <= colEnd):
+            for i in range(colStart, colEnd + 1): # Towards right
+                result.append(matrix[rowStart][i])
+            rowStart += 1
+            for j in range(rowStart, rowEnd + 1): # towards bottom
+                result.append(matrix[j][colEnd])
+            colEnd -= 1
+            if colEnd >= colStart:                
+                for k in range(colEnd, colStart - 1, -1): # towards left
+                    result.append(matrix[rowEnd][k])
+                rowEnd -= 1
+            if rowEnd >= rowStart:
+                for x in range(rowEnd, rowStart - 1, -1): # towards top
+                    result.append(matrix[x][colStart])
+                colStart += 1                
+        return result                
