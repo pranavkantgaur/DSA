0a1b52d5bf616b1c3a35f8c3f40400f0496b1860
pranav <pranav8iisc@gmail.com>
2022-06-17 11:05:49 +0530

    fixed program state check

--- src/algorithms/spiral-matrix.py
+++ src/algorithms/spiral-matrix.py
@@ -17,19 +17,20 @@ class Solution:
         colEnd = len(matrix[0]) - 1
         result = []
         while(rowStart <= rowEnd and colStart <= colEnd):
+            print(rowStart, rowEnd, colStart, colEnd)
             for i in range(colStart, colEnd + 1): # Towards right
                 result.append(matrix[rowStart][i])
             rowStart += 1
-            for j in range(rowStart, rowEnd + 1): # towards bottom
+            for j in range(rowStart, rowEnd + 1): # towards bottom                
                 result.append(matrix[j][colEnd])
             colEnd -= 1
-            if rowEnd >= rowStart:
-                for x in range(rowEnd, rowStart - 1, -1): # towards top
-                    result.append(matrix[x][colStart])
-            colStart += 1                
-            if colEnd >= colStart:                
+            if rowStart <= rowEnd:                
                 for k in range(colEnd, colStart - 1, -1): # towards left
                     result.append(matrix[rowEnd][k])
-            rowEnd -= 1                
-                
+            rowEnd -= 1             
+            if colEnd >= colStart:                
+                for x in range(rowEnd, rowStart - 1, -1): # towards top
+                    result.append(matrix[x][colStart])
+            colStart += 1      
+   
         return result                
