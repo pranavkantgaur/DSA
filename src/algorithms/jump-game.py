d03e6fe3ed1f8d1cd18533c3056fd14ac60ecd41
pranav <pranav8iisc@gmail.com>
2022-06-20 12:27:54 +0530

    WIP: memoization approach

--- src/algorithms/jump-game.py
+++ src/algorithms/jump-game.py
@@ -18,3 +18,25 @@ class Solution:
             else:
                 return False
         return False   
+    
+  class SolutionWithMemoization:      
+    jM = {} # set this to -1 for all positions of input array
+    
+    def canJump(self, nums: List[int]) -> bool:
+        if len(nums)==1:            
+            return True
+        if nums[0] == 0:
+            return False
+        for k in range(1, nums[0] + 1): # evaluate all actions at this state
+            if k <= len(nums):
+                if jM[k] != -1:
+                    if jm[k] == 1:
+                        jm[i] = 1
+                        return True
+                    else:
+                        continue                    
+                else:
+                    jM[k] = self.canJump(nums, k, n)                    
+            else:
+                return False
+        return False                
