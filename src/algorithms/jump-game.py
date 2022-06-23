b2a45374fefd43135f92879889a9095660db9a40
pranav <pranav8iisc@gmail.com>
2022-06-23 10:47:55 +0530

    memoization solution, still TLE

--- src/algorithms/jump-game.py
+++ src/algorithms/jump-game.py
@@ -1,25 +1,5 @@
 # https://leetcode.com/problems/jump-game/
-'''
-Base condition: if nextpos == lastpos: return True, if nextpos > lastpos: return false
-for each nextpos in range(a[I]): if jumpgame(nextpos, lastpos): return true.   Outside for loop: return false
-'''
-class Solution:
-    def canJump(self, nums: List[int]) -> bool:
-        if len(nums) == 1:
-            return True
-        if nums[0] == 0:
-            return False
-        for k in range(1, nums[0] + 1): # evaluate all actions at this state
-            if k < len(nums):
-                if self.canJump(nums[k:]):
-                    return True
-                else:
-                    continue
-            else:
-                return False
-        return False   
-    
-  class SolutionWithMemoization:      
+class Solution:      
     def jgUtil(self, start, end, nums, jg_map):
         if start ==  end:
             jg_map[start] =  1 # true
@@ -32,7 +12,7 @@ class Solution:
                 if jg_map[action + start] == 1:
                     jg_map[start] = 1
                     return True
-                if jg_map[action + start] == 0:
+                elif jg_map[action + start] == 0:
                     continue
                 else:
                     if self.jgUtil(start + action, end, nums, jg_map) == True:
@@ -46,9 +26,8 @@ class Solution:
                     
     
     def canJump(self, nums: List[int]) -> bool:
-        jg_map = {} #set to all -1s for len(nums), TODO
+        jg_map = [-1] * len(nums) #set to all -1s for len(nums), TODO
         # REFER: https://stackoverflow.com/questions/1298636/how-to-set-initial-size-for-a-dictionary-in-python
         start = 0
         end = len(nums) - 1
-        self.jgUtil(start, end, nums, jg_map)
-                
+        return self.jgUtil(start, end, nums, jg_map)
