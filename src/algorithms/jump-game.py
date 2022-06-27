ed69070f0feb6e2e9235fcba9373c899c7f2faec
pranav <pranav8iisc@gmail.com>
2022-06-27 11:44:39 +0530

    wip: wrote discuss soln

--- src/algorithms/jump-game.py
+++ src/algorithms/jump-game.py
@@ -1,33 +1,17 @@
 # https://leetcode.com/problems/jump-game/
-class Solution:      
-    def jgUtil(self, start, end, nums, jg_map):
-        if start ==  end:
-            jg_map[start] =  1 # true
-            return True
-        if nums[start] == 0:
-            jg_map[start] = 0
-            return False
-        for action in range(1, nums[start] + 1):
-            if action + start <= end:
-                if jg_map[action + start] == 1:
-                    jg_map[start] = 1
-                    return True
-                elif jg_map[action + start] == 0:
-                    continue
-                else:
-                    if self.jgUtil(start + action, end, nums, jg_map) == True:
-                        jg_map[start] = 1
-                        return True
-                    else:
-                        continue
+# https://leetcode.com/problems/jump-game/discuss/20917/Linear-and-simple-solution-in-C%2B%2B
+class Solution:                     
+    def canJump(self, nums: List[int]) -> bool:
+        n = len(nums) # 5
+        reach = 0
+        i = 0
+        for i in range(n): # [0, 1, 2, 3, 4]
+            #print(i)
+            if  i <=  reach: # 0 <= 
+                reach = max(i + nums[i], reach)
             else:
                 break
-        return False            
-                    
-    
-    def canJump(self, nums: List[int]) -> bool:
-        jg_map = [-1] * len(nums) #set to all -1s for len(nums), TODO
-        # REFER: https://stackoverflow.com/questions/1298636/how-to-set-initial-size-for-a-dictionary-in-python
-        start = 0
-        end = len(nums) - 1
-        return self.jgUtil(start, end, nums, jg_map)
+        print('val: ', i)
+        return i == n - 1            
+        
+
