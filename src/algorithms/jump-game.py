d25d2d28975f41de86bffa433f353c962012f20d
pranav <pranav8iisc@gmail.com>
2022-06-28 11:31:14 +0530

    ac solution

--- src/algorithms/jump-game.py
+++ src/algorithms/jump-game.py
@@ -2,16 +2,18 @@
 # https://leetcode.com/problems/jump-game/discuss/20917/Linear-and-simple-solution-in-C%2B%2B
 class Solution:                     
     def canJump(self, nums: List[int]) -> bool:
-        n = len(nums) # 5
+        n = len(nums)
+        if n == 1:
+            return True
         reach = 0
         i = 0
-        for i in range(n): # [0, 1, 2, 3, 4]
-            #print(i)
+        for i in range(n): 
             if  i <=  reach: # 0 <= 
                 reach = max(i + nums[i], reach)
             else:
                 break
-        print('val: ', i)
-        return i == n - 1            
+        return i <= reach and i == n - 1 # to avoid cases where i > reach at last element of nums           
+        
+        
         
 
