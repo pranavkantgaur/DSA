9892e049a3060ea32c7e1fabc8271419aab11c21
pranav <pranav8iisc@gmail.com>
2022-06-29 11:32:55 +0530

    wip: fix interval merging util

--- src/algorithms/merge-intervals.py
+++ src/algorithms/merge-intervals.py
@@ -2,7 +2,8 @@
 class Solution:
     
     def mergeUtil(self, index, intervals):
-        new_interval = [intervals[index][0], intervals[index + 1][1]]
+        new_interval = [min(intervals[index][0],intervals[index + 1][0]) \
+                        , max(intervals[index][1], intervals[index + 1][1])]
         intervals.pop(index)
         intervals.pop(index)
         intervals.insert(index, new_interval)
@@ -11,8 +12,10 @@ class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         for index in range(len(intervals)):
             if index + 1 <= len(intervals) - 1:
-                if intervals[index][1] >= intervals[index][0]:
+                if intervals[index][1] >= intervals[index + 1][0]:
                     self.mergeUtil(index, intervals)
+                    index  = max(0, index - 1)
+                    print('Index: ', index)
                 else:
                     continue
         return intervals                    
