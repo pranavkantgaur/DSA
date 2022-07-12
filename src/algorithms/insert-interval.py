16c96af92c67b63904fdaa1d8bc4a92b9eb69452
pranav <pranav8iisc@gmail.com>
2022-07-12 10:35:27 +0530

    WIP: look for overlap and merge approach

--- src/algorithms/insert-interval.py
+++ src/algorithms/insert-interval.py
@@ -1,9 +1,20 @@
 # https://leetcode.com/problems/insert-interval/
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        '''
-        1. Binary search both start_new and end_new of newInterval in intervals to get indices i and j
-        2. Remove all intervals from intervals list between indices i and j(including i, j)
-        3. Insert new Interval as [min(start_i, start_new), max(end_j, end_new)]
-        4. Return intervals
-        '''
+        for index, interval in enumerate(intervals):
+            if self.isOveralapping(newInterval, interval):
+                newInterval = self.mergeInterval(newInterval, interval) # update newInterval
+                new_interval_index = index # mark the index
+                intervals.pop(index) # remove the interval
+            else:
+                continue # look for overlap
+        if new_interval_index is not None:
+            intervals.push(new_interval_index, newInterval)                
+        else: # newInterval did not overlap with input intervals
+            # it should be added to either of the ends of intervals
+            if intervals[0][0] > newIntervals[1]:
+                intervals.insert(0, newInterval)
+            else:
+                intervals.insert(len(intervals) - 1, newInterval)
+            
+        return intervals
