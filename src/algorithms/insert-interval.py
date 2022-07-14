8c732dc9ceecc30c0fb7f2038015335b8fcdde66
pranav <pranav8iisc@gmail.com>
2022-07-14 11:23:00 +0530

    delete intervals after iteration

--- src/algorithms/insert-interval.py
+++ src/algorithms/insert-interval.py
@@ -1,6 +1,6 @@
 # https://leetcode.com/problems/insert-interval/
 class Solution:
-    def isOverlapping(self, interval1, interval2):
+    def isOverlapping(self, interval1, interval2): # add interval endpoint equality cases
         if interval2[0] < interval1[0] and interval1[0] < interval2[1] < interval1[1]:
             return True
         if interval1[0] < interval2[0] and interval2[1] < interval1[1]:
@@ -15,18 +15,20 @@ class Solution:
     def mergeIntervals(self, interval1, interval2):
         newInterval = [min(interval1[0], interval2[0]), max(interval1[1], interval2[1])]
         return newInterval
-    
-    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        new_interval_index = None
+        def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        indices_to_delete = []
         for index, interval in enumerate(intervals):
-            if self.isOverlapping(newInterval, interval):
+            if self.isOverlapping(newInterval, interval): 
                 newInterval = self.mergeIntervals(newInterval, interval) # update newInterval
-                new_interval_index = index # mark the index
-                intervals.pop(index) # remove the interval
-                break
+                indices_to_delete.append(index)
+                print('TO be deleted;', indices_to_delete)
             else:
                 continue # look for overlap
-        if new_interval_index is not None:
+        #if new_interval_index is not None:
+        if indices_to_delete is not None:
+            new_interval_index = indices_to_delete[0]
+            for i in range(len(indices_to_delete)):
+                intervals.pop(new_interval_index)
             intervals.insert(new_interval_index, newInterval)                
         else: # newInterval did not overlap with input intervals
             # it should be added to either of the ends of intervals
