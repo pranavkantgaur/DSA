01269edc5960da07d9d58e31215d511ef4a25837
pranav <pranav8iisc@gmail.com>
2022-08-05 18:20:48 +0530

    add tabulation solution working on std test cases

--- src/algorithms/unique-paths.py
+++ src/algorithms/unique-paths.py
@@ -1,20 +1,22 @@
 # https://leetcode.com/problems/unique-paths/
 class Solution:
     def upUtils(self, m, n, upMap):
-        if m == 1 or n == 1:
-            upMap[m][n] = 1
-            return 
         for row in range(m):
             for col in range(n):
-                upMap[m][n] = upMap[m - 1][n] + upMap[m][n - 1]
+                if row == 0 or col == 0:
+                    upMap[row][col] = 1                
+        for row in range(1, m):
+            for col in range(1, n):
+                upMap[row][col] = upMap[row - 1][col] + upMap[row][col - 1]
         return                
         
     def uniquePaths(self, m: int, n: int) -> int:
         upMap = [[] for row in range(m)]        
         for row in range(m):            
-            upMap[row] = [-1 for col in range(n)]
-            print('LenL ', len(upMap[row]))
+            upMap[row] = [0 for col in range(n)]
         self.upUtils(m , n, upMap)
-        return upMap[m][n]
+        return upMap[m - 1][n - 1]
+        
+        
         
         
