120e6d9f8a055f00483f4205522e861ee1dde08e
pranav <pranav8iisc@gmail.com>
2022-08-18 11:29:30 +0530

    extend zeroing out to entire row/col

--- src/algorithms/set-matrix-zeros.py
+++ src/algorithms/set-matrix-zeros.py
@@ -1,15 +1,15 @@
 # https://leetcode.com/problems/set-matrix-zeroes/
 class Solution:
-    def setRowColMapZero(self, is_seed, row, col, matrix):
+    def setRowColZero(self, row, col, matrix):
         # sets elements as zero along entire row and colm
         i = 0
-        while(i < row):
-            if matrix[i][col] != 0 and is_seed[i][col] == 0:
+        while(i < len(matrix)):
+            if matrix[i][col] != 0:
                 matrix[i][col] = 0
             i+=1
         j = 0
-        while(j < col):
-            if matrix[row][j] != 0 and is_seed[row][j] == 0:
+        while(j < len(matrix[0])):
+            if matrix[row][j] != 0:
                 matrix[row][j] = 0
             j+=1
                     
@@ -21,17 +21,17 @@ class Solution:
         """
         n_rows = len(matrix)
         n_cols = len(matrix[0])
-        is_seed = [[] for row in range(n_rows)]
+        is_seed = [[] for row in range(n_rows)]        
         for row in range(n_rows):
-            is_seed[row] = [1 if matrix[row][col] == 0 else 0 for col in range(n_cols)]
-        
+            is_seed[row] = [1 if matrix[row][col] == 0 else 0 for col in range(n_cols)]        
         for row in range(n_rows):
             for col in range(n_cols):
                 if matrix[row][col] == 0 and is_seed[row][col] == 1:
-                    self.setRowColMapZero(is_seed, row, col, matrix)                                        
+                    self.setRowColZero(row, col, matrix)                                        
                 else:
                     continue
                     
+                            
         
                     
         
