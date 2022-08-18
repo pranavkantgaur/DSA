1491d4e5a2c1578f4d55c0d835c9b1a55e8374da
pranav <pranav8iisc@gmail.com>
2022-08-18 11:20:13 +0530

    WIP: add helper method for setting row-col zeros

--- src/algorithms/set-matrix-zeros.py
+++ src/algorithms/set-matrix-zeros.py
@@ -1,26 +1,38 @@
 # https://leetcode.com/problems/set-matrix-zeroes/
 class Solution:
+    def setRowColMapZero(self, is_seed, row, col, matrix):
+        # sets elements as zero along entire row and colm
+        i = 0
+        while(i < row):
+            if matrix[i][col] != 0 and is_seed[i][col] == 0:
+                matrix[i][col] = 0
+            i+=1
+        j = 0
+        while(j < col):
+            if matrix[row][j] != 0 and is_seed[row][j] == 0:
+                matrix[row][j] = 0
+            j+=1
+                    
+                
+        # also updates the is_seed map if is_seed
     def setZeroes(self, matrix: List[List[int]]) -> None:
         """
         Do not return anything, modify matrix in-place instead.
         """
         n_rows = len(matrix)
         n_cols = len(matrix[0])
-        marked_zero = [[] for row in range(n_rows)]
+        is_seed = [[] for row in range(n_rows)]
         for row in range(n_rows):
-            marked_zero[row] = [1 for col in range(n_cols)]
+            is_seed[row] = [1 if matrix[row][col] == 0 else 0 for col in range(n_cols)]
         
         for row in range(n_rows):
             for col in range(n_cols):
-                if matrix[row][col] == 0:
-                    self.mark_row_col_zero(marked_zero, row, col)                    
+                if matrix[row][col] == 0 and is_seed[row][col] == 1:
+                    self.setRowColMapZero(is_seed, row, col, matrix)                                        
                 else:
                     continue
-                            
-        for row in range(n_rows):
-            for col in range(n_cols):
-                if marked_zero[row][col]:
-                    matrix[row][col] = 0
+                    
+        
                     
         
                     
