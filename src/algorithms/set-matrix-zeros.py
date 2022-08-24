7cb060a7773765a19b79a416cf16402ee22117be
pranav <pranav8iisc@gmail.com>
2022-08-24 18:23:37 +0530

    add o(m + n) solution

--- src/algorithms/set-matrix-zeros.py
+++ src/algorithms/set-matrix-zeros.py
@@ -20,17 +20,34 @@ class Solution:
         Do not return anything, modify matrix in-place instead.
         """
         n_rows = len(matrix)
-        n_cols = len(matrix[0])
-        is_seed = [[] for row in range(n_rows)]        
-        for row in range(n_rows):
-            is_seed[row] = [1 if matrix[row][col] == 0 else 0 for col in range(n_cols)]        
+        n_cols = len(matrix[0])        
+        
+        # row major
+        is_seed_row = [0 for row in range(n_rows)]
         for row in range(n_rows):
             for col in range(n_cols):
-                if matrix[row][col] == 0 and is_seed[row][col] == 1:
+                if matrix[row][col] == 0:
+                    is_seed_row[row] = 1
+                    break
+                else:
+                    continue
+        # col major
+        is_seed_col = [0 for col in range(n_cols)]
+        for col in range(n_cols):
+            for row in range(n_rows):
+                if matrix[row][col] == 0:
+                    is_seed_col[col] = 1
+                    break
+                else:
+                    continue            
+        for row in range(n_rows):
+            for col in range(n_cols):
+                if is_seed_row[row] == 1 and is_seed_col[col] == 1:
                     self.setRowColZero(row, col, matrix)                                        
                 else:
                     continue
                     
+                            
                             
         
                     
