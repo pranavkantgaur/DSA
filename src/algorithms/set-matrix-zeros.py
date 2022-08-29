45673945e06b92fe7969dd44fc97300445416a16
pranav <pranav8iisc@gmail.com>
2022-08-29 11:24:09 +0530

    WIP: O(1) space solution

--- src/algorithms/set-matrix-zeros.py
+++ src/algorithms/set-matrix-zeros.py
@@ -1,52 +1,58 @@
 # https://leetcode.com/problems/set-matrix-zeroes/
-class Solution:
-    def setRowColZero(self, row, col, matrix):
-        # sets elements as zero along entire row and colm
-        i = 0
-        while(i < len(matrix)):
-            if matrix[i][col] != 0:
-                matrix[i][col] = 0
-            i+=1
-        j = 0
-        while(j < len(matrix[0])):
-            if matrix[row][j] != 0:
-                matrix[row][j] = 0
-            j+=1
-                    
-                
-        # also updates the is_seed map if is_seed
+class Solution:                
     def setZeroes(self, matrix: List[List[int]]) -> None:
         """
         Do not return anything, modify matrix in-place instead.
         """
         n_rows = len(matrix)
         n_cols = len(matrix[0])        
+        row0 = -1
+        col0 = -1
         
-        # row major
-        is_seed_row = [0 for row in range(n_rows)]
+        for col in range(n_cols):
+            if matrix[0][col] == 0:
+                row0 = 0
+                break
+                
         for row in range(n_rows):
+            if matrix[row][0] == 0:
+                col0 = 0
+                break
+                
+        for row in range(1, n_rows):
             for col in range(n_cols):
                 if matrix[row][col] == 0:
-                    is_seed_row[row] = 1
+                    matrix[row][0] = 0
                     break
-                else:
-                    continue
-        # col major
-        is_seed_col = [0 for col in range(n_cols)]
-        for col in range(n_cols):
+                         
+        for col in range(1, n_cols):
             for row in range(n_rows):
                 if matrix[row][col] == 0:
-                    is_seed_col[col] = 1
-                    break
-                else:
-                    continue            
-        for row in range(n_rows):
+                    matrix[0][col] = 0
+                    break                        
+
+        for row in range(1, n_rows):            
+            if matrix[row][0] == 0:
+                for col in range(1 , n_cols):
+                    matrix[row][col] = 0
+        
+        for col in range(1, n_cols):
+            if matrix[0][col] == 0:
+                for row in range(1 , n_rows):
+                    matrix[row][col] = 0
+    
+        if row0 == 0:
             for col in range(n_cols):
-                if is_seed_row[row] == 1 and is_seed_col[col] == 1:
-                    self.setRowColZero(row, col, matrix)                                        
-                else:
-                    continue
+                matrix[0][col] = 0 
+        if col0 == 0:
+            for row in range(n_rows):
+                matrix[row][0] = 0 
+            
+            
+                
+                
                     
+                            
                             
                             
         
