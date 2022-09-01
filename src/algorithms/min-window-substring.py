7404dac5779ea59ec059575c9c896ad5ee110a1a
pranav <pranav8iisc@gmail.com>
2022-09-01 11:35:40 +0530

    WIP: add algorithm description

--- src/algorithms/min-window-substring.py
+++ src/algorithms/min-window-substring.py
@@ -2,27 +2,32 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
         start = 0
-        end = 0
-        # get frequency of letters in target string, t_map
-        # while end does not reach the end of string:
-          # update start and end untill s[start] does not belong to t_map
-          # once start points to a letter contained in t_map, check if s_map[start:end] match t_map
-            # if not continue updating end ptr and checking if s_map[start:end] match t_map
-               # if there is a match, update min_substring so far and update start to next letter(in t_map)
-               # if there is no match, continue updating end pointers 
+        end = len(t) - 1
+        if len(t) > len(s):
+            return ""
         t_map = collections.Counter(t) # maintains count of letters in 't'
+        # fix start
         while(end < len(s)):
-            start = self._update_start()
-            end = start
-            if collections.Counter(s[start : end]) == t_map:
-                if min_subs_len_so_far > end - start:
-                    min_subs_len_so_far = end - start
-                    min_start = start            
-                    start = self._update_start()                            
+            if s[start] is not in t_map:
+                start += 1
                 end += 1
-                        
-        return s[start:end]            
+        # update end
+        while(end < len(s)):
+            if s[start:end] != t_map:
+                end += 1
+            else:
+                if min_len_so_far > end - start + 1:
+                    min_len_so_far = end - start + 1
+                    min_start = start
+                    end += 1
+                    # TODO, go to first while loop
+                else:
+                    end += 1
+                    
+                
+        return s[min_start:min_start + min_length_so_far]            
             
+                    
         
             
         
