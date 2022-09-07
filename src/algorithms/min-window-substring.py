17fdbd96751632aec98a6fc37d7689b2a28e640b
pranav <pranav8iisc@gmail.com>
2022-09-07 11:37:30 +0530

    add helper for advancing start end ptrs

--- src/algorithms/min-window-substring.py
+++ src/algorithms/min-window-substring.py
@@ -1,17 +1,21 @@
-# https://leetcode.com/problems/minimum-window-substring/
-class Solution:
+# https://leetcode.com/problems/minimum-window-substring/class Solution:
+    def advanceStartEndPtr(self, start, end, s, t_map):
+        print('keys: ', t_map.keys())
+        while(end < len(s)):
+            if s[start] is not in t_map.keys():
+                start += 1
+                end += 1
+                        
+        
+        
     def minWindow(self, s: str, t: str) -> str:
         start = 0
         end = len(t) - 1
         if len(t) > len(s):
             return ""
         t_map = collections.Counter(t) # maintains count of letters in 't'
-        # fix start
-        while(end < len(s)):
-            if s[start] is not in t_map:
-                start += 1
-                end += 1
-        # update end
+        min_start = 0
+        min_length_so_far = len(s)
         while(end < len(s)):
             if s[start:end] != t_map:
                 end += 1
@@ -19,13 +23,15 @@ class Solution:
                 if min_len_so_far > end - start + 1:
                     min_len_so_far = end - start + 1
                     min_start = start
-                    end += 1
-                    # TODO, go to first while loop
-                else:
-                    end += 1
+                self.advanceStartEndPtr(start, end, s, t_map)
+
                     
-                
-        return s[min_start:min_start + min_length_so_far]            
+        if min_length_so_far == len(s): # no candidate substring found
+            return ""
+        else:
+            return s[min_start:min_start + min_length_so_far]            
+            
+                    
             
                     
         
