41cbfafa93e268f5718ea70dd155d51cf6c64690
pranav <pranav8iisc@gmail.com>
2022-09-12 11:18:16 +0530

    fixed syntax

--- src/algorithms/min-window-substring.py
+++ src/algorithms/min-window-substring.py
@@ -1,11 +1,17 @@
 # https://leetcode.com/problems/minimum-window-substring/class Solution:
+class Solution:
     def advanceStartEndPtr(self, start, end, s, t_map):
-        print('keys: ', t_map.keys())
+        #print('keys: ', t_map.keys())
+        #print('s', s[start])        
+        
         while(end < len(s)):
-            if s[start] is not in t_map.keys():
+            if s[start] not in t_map.keys():
                 start += 1
                 end += 1
-                        
+            else:
+                break
+        #print('start: ', start) 
+        return start, end
         
         
     def minWindow(self, s: str, t: str) -> str:
@@ -15,22 +21,28 @@
             return ""
         t_map = collections.Counter(t) # maintains count of letters in 't'
         min_start = 0
-        min_length_so_far = len(s)
-        while(end < len(s)):
-            if s[start:end] != t_map:
+        min_len_so_far = len(s)
+        start, end = self.advanceStartEndPtr(start, end, s, t_map)
+        #print('START: ', start)
+        #print('End: ', end)        
+        
+        while(end < len(s)): # TODO: infinite loop fix it
+            if collections.Counter(s[start:end]) - t_map is not None: # https://stackoverflow.com/a/52324065
                 end += 1
             else:
                 if min_len_so_far > end - start + 1:
                     min_len_so_far = end - start + 1
                     min_start = start
-                self.advanceStartEndPtr(start, end, s, t_map)
+                start, end = self.advanceStartEndPtr(start, end, s, t_map)
 
                     
-        if min_length_so_far == len(s): # no candidate substring found
+        if min_len_so_far == len(s): # no candidate substring found
             return ""
         else:
-            return s[min_start:min_start + min_length_so_far]            
+            return s[min_start:min_start + min_len_so_far]            
+                    
             
+                    
                     
             
                     
