f5ab9c975a76b4888a2ca45fe175347390feb0ea
pranav <pranav8iisc@gmail.com>
2022-09-28 11:37:18 +0530

    WIP: refactor substring matching cases

--- src/algorithms/min-window-substring.py
+++ src/algorithms/min-window-substring.py
@@ -1,5 +1,15 @@
 # https://leetcode.com/problems/minimum-window-substring/class Solution:
-class Solution:       
+class Solution:     
+        
+    def substringMatchesCounter(self, substring, counter):
+        return Counter(substring) - counter == 0
+    
+    def updateMin(self, start, end, min_start, min_length):
+        if end - start < min_length: 
+            return start, end - start 
+        else: 
+            return min_start, min_length
+    
     def minWindow(self, s: str, t: str) -> str:
         start = 0
         end = len(t)
@@ -9,25 +19,24 @@ class Solution:
         min_start = 0
         min_len_so_far = len(s) + 1 
         
-        while(end < len(s)): 
-            if len(t_map - collections.Counter(s[start:end])) == 0: # https://stackoverflow.com/a/52324065
-                # update min length substring
-                print('Match: ', s[start:end])
-                if min_len_so_far > end - start:
-                    min_len_so_far = end - start
-                    min_start = start        
+        while(!(end == len(s) and not self.substringMatchesCounter(s[start:end], t_map))):
+            if end == len(s) and self.substringMatchesCounter(s[start:end], t_map):
+                min_start, min_length = self.updateMin(start, end, min_start, min_length)
+                if end - start > len(t):
+                    start += 1
+                else:
+                    break                                
+            if end != len(s) and not self.substringMatchesCounter(s[start:end], t_map):
+                end += 1
+            if end != len(s) and self.substringMatchesCounter(s[start:end], t_map):
+                min_start, min_length = self.updateMin(start, end, min_start, min_length)
                 start += 1
-            if end - start > len(t) and end + 1 == len(s):
-                pass
-            else:
-                end += 1                  
-
+                end += 1
                     
         if min_len_so_far == len(s) + 1: # no candidate substring found
             return ""
         else:
-            return s[min_start:min_start + min_len_so_far]            
-                    
+            return s[min_start:min_start + min_len_so_far]                    
             
                             
             
