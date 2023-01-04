8487968839d975e184454070598d8ea89519b5ff
pranav <pranav8iisc@gmail.com>
2023-01-04 12:41:52 +0530

    WIP: fixes helper call from main function

--- src/algorithms/word-search.py
+++ src/algorithms/word-search.py
@@ -1,26 +1,41 @@
 # https://leetcode.com/problems/word-search/
 class Solution:
-    def exist_helper(self, board, word, seed):
-        if word is None:
+    def helper(board, word, letter_index, start_pos):
+        if letter_index == len(word):
             return True
-        starting = 0
-        for row in len(board):
-            for col in len(board[0]):
-                if word[starting] == board[row][col]:
-                    # check in all neighbors for the match
-                    visited[row][col] = True
-                    neighbor_list = [(row - 1, col), (row, col - 1), (row, col + 1), (row + 1, col)]
-                    for neighbor in neighbor_list:                        
-                        if visited[neighbor[0]][neighbor[1]] == False:
-                            found = self.exist_helper(board, word[start + 1], seed = neighbor)
-                            if found is True:
-                                return True
-                            else:
-                                continue    
-                        else:
-                            continue
+        if word[letter_index] != board[start_pos[0]][start_pos[1]]:
+            return False
+        else:
+            visited[start_pos[0]][start_pos[1]] = True            
+            neighbors = [[start_pos[0] - 1, start_pos[1]], [start_pos[0], start_pos[1] - 1], [start_pos[0], start_pos[1] + 1], [start_pos[0] + 1, start_pos[1]]]
+            for neighhbor in neighbors:
+                if len(board[0]) - 1 < neighbor[1] or neighbor[1] < 0:
+                    continue
+                if len(board) - 1 < neighbor[0] or neighbor[0] < 0:
+                    continue
+                # else it is a valid neighbor
+                if visited[neighbor[0]][neighbor[1]] == False:
+                    if self.helper(board, word, letter_index + 1, start_pos = neighbor) == True:
+                        return True
+                    else:                        
+                        continue
                 else:
-                    continue        
+                    continue
+            return False            
+               
+       
 
-    def exist(self, board: List[List[str]], word: str) -> bool:
-        self.exist_helper()
+    def exist(board, word):
+        m = len(board)
+        n = len(board[0])
+        if len(word) > m * n:
+            return False
+        i = 0
+        for row in range(m):
+            for col in range(n):
+                if helper(board, word, i, start = [row, col]) == True:
+                    return True
+        return False       
+    
+    
+ 
