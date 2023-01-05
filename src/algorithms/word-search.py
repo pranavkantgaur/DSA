0f665ae8d37b6c7d8c22863a1e8eba86b38fb8a0
pranav <pranav8iisc@gmail.com>
2023-01-05 12:46:12 +0530

    add check for out of bound start_pos

--- src/algorithms/word-search.py
+++ src/algorithms/word-search.py
@@ -1,17 +1,21 @@
 # https://leetcode.com/problems/word-search/
+'''
+Notes from: https://leetcode.com/problems/word-search/solutions/27658/accepted-very-short-java-solution-no-additional-space/?orderBy=most_votes
+* Uses bit-mask to store visited map in the sign-bit of each element of the board(which is a A-Z character, hence the first bit is don't care).
+  * board[x][y] ^= 256 (100000000) if (x, y) is visited.
+'''
+
 class Solution:
     def helper(board, word, letter_index, start_pos):
         if letter_index == len(word):
             return True
-        if word[letter_index] != board[start_pos[0]][start_pos[1]]:
+        if start_pos[0] > len(board) - 1 or start_pos[0] < 0 or start_pos[1] > len(board[0] - 1) or start_pos[1] < 0 or word[letter_index] != board[start_pos[0]][start_pos[1]]: 
             return False
         else:
-            visited[start_pos[0]][start_pos[1]] = True            
+            visited[start_pos[0]][start_pos[1]] = True        
             neighbors = [[start_pos[0] - 1, start_pos[1]], [start_pos[0], start_pos[1] - 1], [start_pos[0], start_pos[1] + 1], [start_pos[0] + 1, start_pos[1]]]
             for neighhbor in neighbors:
-                if len(board[0]) - 1 < neighbor[1] or neighbor[1] < 0:
-                    continue
-                if len(board) - 1 < neighbor[0] or neighbor[0] < 0:
+                if len(board[0]) - 1 < neighbor[1] or neighbor[1] < 0 or len(board) - 1 < neighbor[0] or neighbor[0] < 0: 
                     continue
                 # else it is a valid neighbor
                 if visited[neighbor[0]][neighbor[1]] == False:
