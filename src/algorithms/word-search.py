9f22bd6c3c9b177281ddf3ba7bd3319819006eed
pranav <pranav8iisc@gmail.com>
2023-03-24 16:09:52 +0530

    remove bit setting

--- src/algorithms/word-search.py
+++ src/algorithms/word-search.py
@@ -6,6 +6,34 @@ Notes from: https://leetcode.com/problems/word-search/solutions/27658/accepted-v
 '''
 
 class Solution:
+class Solution:
+    
+    
+    '''
+    def exist(self, board, word):
+        if not board:
+            return False
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if self.dfs(board, i, j, word):
+                    return True
+        return False
+
+    # check whether can find word, start at (i,j) position    
+    def dfs(self, board, i, j, word):
+        if len(word) == 0: # all the characters are checked
+            return True
+        if i<0 or i>=len(board) or j<0 or j>=len(board[0]) or word[0]!=board[i][j]:
+            return False
+        tmp = board[i][j]  # first character is found, check the remaining part
+        board[i][j] = "#"  # avoid visit agian 
+        # check whether can find "word" along one direction
+        res = self.dfs(board, i+1, j, word[1:]) or self.dfs(board, i-1, j, word[1:]) \
+        or self.dfs(board, i, j+1, word[1:]) or self.dfs(board, i, j-1, word[1:])
+        board[i][j] = tmp
+        return res
+    '''
+    
     def helper(self, board, word, letter_index, start_pos):
         if letter_index == len(word):
             return True
@@ -13,22 +41,13 @@ class Solution:
             return False
         if letter_index == len(word) - 1:
             return True
-        else:
-            board[start_pos[0]][start_pos[1]] = chr(ord(board[start_pos[0]][start_pos[1]])^ 256)        
+        else:            
+            temp = board[start_pos[0]][start_pos[1]]
+            board[start_pos[0]][start_pos[1]] = '#'
             neighbors = [[start_pos[0] - 1, start_pos[1]], [start_pos[0], start_pos[1] - 1], [start_pos[0], start_pos[1] + 1], [start_pos[0] + 1, start_pos[1]]]
-            for neighbor in neighbors:
-                if len(board[0]) - 1 < neighbor[1] or neighbor[1] < 0 or len(board) - 1 < neighbor[0] or neighbor[0] < 0: 
-                    continue
-                # else it is a valid neighbor
-                if ord(board[neighbor[0]][neighbor[1]]) & 256 == 0:
-                    if self.helper(board, word, letter_index + 1, start_pos = neighbor) == True:
-                        return True
-                    else:                        
-                        continue
-                else:
-                    continue
-            board[start_pos[0]][start_pos[1]] = chr(ord(board[start_pos[0]][start_pos[1]])^ 256)
-            return False            
+            result = self.helper(board, word, letter_index + 1, start_pos = neighbors[0]) or self.helper(board, word, letter_index + 1, start_pos = neighbors[1]) or self.helper(board, word, letter_index + 1, start_pos = neighbors[2]) or self.helper(board, word, letter_index + 1, start_pos = neighbors[3])            
+            board[start_pos[0]][start_pos[1]] = temp            
+            return result
                
        
 
@@ -43,3 +62,4 @@ class Solution:
                 if self.helper(board, word, i, [row, col]) == True:
                     return True
         return False
+          
