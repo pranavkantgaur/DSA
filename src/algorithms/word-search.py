b6fde3b0ce5c410f53ec525d1ddd40c1097b067b
pranav <pranav8iisc@gmail.com>
2023-03-28 14:15:22 +0530

    set neighbor visit order same as forum answer

--- src/algorithms/word-search.py
+++ src/algorithms/word-search.py
@@ -6,31 +6,10 @@ Notes from: https://leetcode.com/problems/word-search/solutions/27658/accepted-v
 '''
 
 class Solution:
- 
-    '''
-    def exist(self, board, word):
-        if not board:
-            return False
-        for i in range(len(board)):
-            for j in range(len(board[0])):
-                if self.dfs(board, i, j, word):
-                    return True
-        return False
+    
+    
 
-    # check whether can find word, start at (i,j) position    
-    def dfs(self, board, i, j, word):
-        if len(word) == 0: # all the characters are checked
-            return True
-        if i<0 or i>=len(board) or j<0 or j>=len(board[0]) or word[0]!=board[i][j]:
-            return False
-        tmp = board[i][j]  # first character is found, check the remaining part
-        board[i][j] = "#"  # avoid visit agian 
-        # check whether can find "word" along one direction
-        res = self.dfs(board, i+1, j, word[1:]) or self.dfs(board, i-1, j, word[1:]) \
-        or self.dfs(board, i, j+1, word[1:]) or self.dfs(board, i, j-1, word[1:])
-        board[i][j] = tmp
-        return res
-    '''
+    
     def helper(self, board, word, letter_index, start_pos):
         if letter_index == len(word):
             return True
@@ -41,10 +20,9 @@ class Solution:
         else:            
             temp = board[start_pos[0]][start_pos[1]]
             board[start_pos[0]][start_pos[1]] = '#'
-            #neighbors = [[start_pos[0] - 1, start_pos[1]], [start_pos[0], start_pos[1] - 1], [start_pos[0], start_pos[1] + 1], [start_pos[0] + 1, start_pos[1]]]
-            #result = self.helper(board, word[1:], 0, start_pos = neighbors[0]) or self.helper(board, word[1:], 0, start_pos = neighbors[1]) or self.helper(board, word[1:], 0, start_pos = neighbors[2]) or self.helper(board, word[1:], 0, start_pos = neighbors[3])            
-            result = self.helper(board, word[1:], 0, start_pos = [start_pos[0] - 1, start_pos[1]]) or self.helper(board, word[1:], 0, start_pos = [start_pos[0], start_pos[1] - 1]) or self.helper(board, word[1:], 0, start_pos = [start_pos[0], start_pos[1] + 1]) or self.helper(board, word[1:], 0, start_pos = [start_pos[0] + 1, start_pos[1]])            
-            #result = self.helper(board, word, letter_index+1, start_pos = neighbors[0]) or self.helper(board, word, letter_index+1, start_pos = neighbors[1]) or self.helper(board, word, letter_index + 1, start_pos = neighbors[2]) or self.helper(board, word, letter_index+1, start_pos = neighbors[3])            
+            result = self.helper(board, word[1:], 0, start_pos = [start_pos[0] + 1, start_pos[1]]) or self.helper(board, word[1:], 0, start_pos = [start_pos[0] - 1, start_pos[1]]) or self.helper(board, word[1:], 0, start_pos = [start_pos[0], start_pos[1] + 1]) or self.helper(board, word[1:], 0, start_pos = [start_pos[0], start_pos[1] - 1])            
+         #         res = self.dfs(board, i+1, j, word[1:]) or self.dfs(board, i-1, j, word[1:]) or self.dfs(board, i, j+1, word[1:]) or self.dfs(board, i, j-1, word[1:])
+
             board[start_pos[0]][start_pos[1]] = temp            
             return result
                
@@ -58,6 +36,30 @@ class Solution:
         i = 0
         for row in range(m):
             for col in range(n):
-                if self.helper(board, word, i, [row, col]) == True:
+                if self.helper(board, word, i, [row, col]):
                     return True
         return False
+    '''
+    def exist(self, board, word):
+        if not board:
+            return False
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if self.dfs(board, i, j, word):
+                    return True
+        return False
+
+# check whether can find word, start at (i,j) position    
+    def dfs(self, board, i, j, word):
+        if len(word) == 0: # all the characters are checked
+            return True
+        if i<0 or i>=len(board) or j<0 or j>=len(board[0]) or word[0]!=board[i][j]:
+            return False
+        tmp = board[i][j]  # first character is found, check the remaining part
+        board[i][j] = "#"  # avoid visit agian 
+        # check whether can find "word" along one direction
+        res = self.dfs(board, i+1, j, word[1:]) or self.dfs(board, i-1, j, word[1:]) \
+        or self.dfs(board, i, j+1, word[1:]) or self.dfs(board, i, j-1, word[1:])
+        board[i][j] = tmp
+        return res          
+    '''        
