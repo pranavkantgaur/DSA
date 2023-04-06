734e1c12bf593684ab6956f6c3469d35d7910cec
pranav <pranav8iisc@gmail.com>
2023-04-06 11:48:45 +0530

    WIP: add search implementation

--- src/algorithms/implement-trie.py
+++ src/algorithms/implement-trie.py
@@ -1,8 +1,14 @@
 # https://leetcode.com/problems/implement-trie-prefix-tree/
 class Trie:
+    class Node:
+        def __init__(self):
+            self.alpha_node_map = {}
+            for letters in englist_alphabet:
+                self.alpha_node_map[letter] = None
+
 
     def __init__(self):
-        
+        self.trie_root = Node
 
     def insert(self, word: str) -> None:
         next_node = self.tree_root
@@ -14,10 +20,21 @@ class Trie:
                 next_node = next_node[letter]
         next_node.is_leaf = True
 
-
-
-
     def search(self, word: str) -> bool:
+        next_node = self.trie_root
+        for letter in word:
+            if letter in next_node.keys():
+                next_node = next_node[letter]
+                if next_node.is_leaf is True:
+                    if letter is last:
+                        return True
+                    else:
+                        return False
+                else:
+                    continue                                
+            else:
+                return False
+        return False # case when no match was found                         
         
 
     def startsWith(self, prefix: str) -> bool:
