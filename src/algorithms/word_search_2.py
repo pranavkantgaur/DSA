9a3c80ae4db89b4e314c5696a6e473e548089d95
pranav <pranav8iisc@gmail.com>
2023-05-02 14:33:52 +0530

    add fixes for checking if a node is a leaf

--- src/algorithms/word_search_2.py
+++ src/algorithms/word_search_2.py
@@ -39,6 +39,8 @@ class Trie(object):
         self.trie_root = TrieNode()
 
     def insert(self, word):
+        if len(word) == 0:
+            return
         current_node = self.trie_root
         for letter in word:
             if current_node.alpha_map[letter] is not None:
@@ -52,16 +54,17 @@ class Trie(object):
         print('Trie is: ')
         queue = []
         current_node = self.trie_root
-        print('Current node: ', current_node.alpha_map)
+        #print('Current node: ', current_node.alpha_map)
         while(True):                        
             for key in current_node.alpha_map.keys():
                 if current_node.alpha_map[key] is not None:
-                    queue.append(key)            
+                    queue.append(current_node.alpha_map[key])            
                     print(key)
                 else:
                     continue        
             if len(queue) != 0:
-                current_node = queue.pop() # take out first element of queue
+                current_node = queue.pop(0) # take out first element of queue
+                print('\n')
             else:
                 break  
         
@@ -73,14 +76,13 @@ class Solution:
         else:
             return False   
 
-    def helper(self, board: List[List[str]], start_x: int, start_y: int, words_trie_node: TrieNode, current_string: str, result: Set[str], visited: List[List[bool]]) -> None:        
+    def helper(self, board: List[List[str]], start_x: int, start_y: int, words_trie_node: TrieNode, current_string: str, result: Set[str], visited: List[List[bool]]) -> None:                
         if words_trie_node.alpha_map[board[start_x][start_y]] is not None: # compare letter on the board with trie
             current_string += board[start_x][start_y]            
             visited[start_x][start_y] = True
-            if words_trie_node.is_leaf == True:
-              result.insert(current_string) # avoids duplicate strings
-            else:
-              #continue
+            if words_trie_node.alpha_map[board[start_x][start_y]].is_leaf == True:
+              result.add(current_string) # avoids duplicate strings              
+            else:              
               pass
             words_trie_node = words_trie_node.alpha_map[board[start_x][start_y]]
             neighbors = [[start_x + 1, start_y], [start_x, start_y + 1], [start_x - 1, start_y], [start_x, start_y - 1]]
@@ -102,7 +104,7 @@ class Solution:
         words_trie = Trie()
         for word in words:
             words_trie.insert(word)
-        words_trie.print()            
+        #words_trie.print()            
         current_string = ""
         for row in range(len(board)):
             for col in range(len(board[0])):
