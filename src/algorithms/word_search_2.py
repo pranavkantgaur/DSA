db2a1669f110954ed3de6c5e104cc7e3dcb5655b
pranav <pranav8iisc@gmail.com>
2023-05-26 14:43:20 +0530

    removed utils functions

--- src/algorithms/word_search_2.py
+++ src/algorithms/word_search_2.py
@@ -19,36 +19,13 @@ class Trie(object):
                 current_node.alpha_map[letter] = TrieNode()
                 current_node = current_node.alpha_map[letter]
         current_node.is_leaf = True
-        self.number_of_words += 1
-
-    def print(self):
-        print('Trie is: ')
-        queue = []
-        current_node = self.trie_root
-        #print('Current node: ', current_node.alpha_map)
-        while(True):                        
-            for key in current_node.alpha_map.keys():
-                if current_node.alpha_map[key] is not None:
-                    queue.append(current_node.alpha_map[key])            
-                    print(key)
-                else:
-                    continue        
-            if len(queue) != 0:
-                current_node = queue.pop(0) # take out first element of queue
-                print('\n')
-            else:
-                break  
-        
+        self.number_of_words += 1       
 
 class Solution:   
-    def is_valid_neighbor(self, neighbor, row_lim, col_lim):
-        if neighbor[0] >= 0 and neighbor[0] < row_lim and neighbor[1] >= 0 and neighbor[1] < col_lim:
-            return True
-        else:
-            return False   
-
     def helper(self, board: List[List[str]], start_row: int, start_col: int, trie_obj: Trie, words_trie_node: TrieNode, current_string: str, result: Set[str], visited: List[List[bool]]) -> None:                
-        if words_trie_node.alpha_map[board[start_row][start_col]] is not None and trie_obj.number_of_words > 0: # compare letter on the board with trie
+        if start_row < 0 or start_row > len(board) - 1 or start_col < 0 or start_col > len(board[0]) - 1 or visited[start_row][start_col] == False or trie_obj.number_of_words == 0 or words_trie_node.alpha_map[board[start_row][start_col]] == None:
+            return
+        else: # compare letter on the board with trie
             current_string += board[start_row][start_col]            
             visited[start_row][start_col] = True
             if words_trie_node.alpha_map[board[start_row][start_col]].is_leaf == True:
@@ -58,28 +35,12 @@ class Solution:
             else:              
               pass
             words_trie_node = words_trie_node.alpha_map[board[start_row][start_col]]
-            neighbors = [[start_row + 1, start_col], [start_row, start_col + 1], [start_row - 1, start_col], [start_row, start_col - 1]]
-            for neighbor in neighbors:
-                if self.is_valid_neighbor(neighbor, len(board), len(board[0])) and visited[neighbor[0]][neighbor[1]] == False:
-                  self.helper(board, neighbor[0], neighbor[1], trie_obj, words_trie_node, current_string, result, visited)
-                else:
-                  continue
+            self.helper(board, start_row + 1, start_col, trie_obj, words_trie_node, current_string, result, visited)
+            self.helper(board, start_row, start_col + 1, trie_obj, words_trie_node, current_string, result, visited)
+            self.helper(board, start_row - 1, start_col, trie_obj, words_trie_node, current_string, result, visited)
+            self.helper(board, start_row, start_col - 1, trie_obj, words_trie_node, current_string, result, visited)
             visited[start_row][start_col] = False                
-        else:
-            return                
         return          
-    def _get_unique_letters(self, input_strings):
-        unique_letter_set = set()
-        for input_string in input_strings:            
-                if len(unique_letter_set) < 26:
-                    for letter in input_string:
-                        if len(unique_letter_set) < 26:
-                            unique_letter_set.add(letter)
-                        else:
-                            break                                    
-                else:
-                    break                            
-        return unique_letter_set
 
 
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
