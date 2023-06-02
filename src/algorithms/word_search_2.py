b7a9218727b7066b1265a63457152c1a3af2fb4e
pranav <pranav8iisc@gmail.com>
2023-06-02 12:08:54 +0530

    remove visited map

--- src/algorithms/word_search_2.py
+++ src/algorithms/word_search_2.py
@@ -19,30 +19,32 @@ class Trie(object):
                 current_node.alpha_map[letter] = TrieNode()
                 current_node = current_node.alpha_map[letter]
         current_node.is_leaf = True
-        self.number_of_words += 1       
+        self.number_of_words += 1        
 
 class Solution:   
-    def helper(self, board: List[List[str]], start_row: int, start_col: int, trie_obj: Trie, words_trie_node: TrieNode, current_string: str, result: Set[str], visited: List[List[bool]]) -> None:                
-        if start_row < 0 or start_row > len(board) - 1 or start_col < 0 or start_col > len(board[0]) - 1 or visited[start_row][start_col] == False or trie_obj.number_of_words == 0 or words_trie_node.alpha_map[board[start_row][start_col]] == None:
-            return
-        else: # compare letter on the board with trie
+    def helper(self, board: List[List[str]], start_row: int, start_col: int, trie_obj: Trie, words_trie_node: TrieNode, current_string: str, result: Set[str]) -> None:                
+        if words_trie_node.alpha_map[board[start_row][start_col]] is not None and trie_obj.number_of_words > 0: # compare letter on the board with trie
             current_string += board[start_row][start_col]            
-            visited[start_row][start_col] = True
+            t = board[start_row][start_col]            
+            board[start_row][start_col] = '#'            
             if words_trie_node.alpha_map[board[start_row][start_col]].is_leaf == True:
               result.add(current_string) # avoids duplicate strings 
-              words_trie_node.alpha_map[board[start_row][start_col]].is_leaf = False
+              words_trie_node.alpha_map[board[start_row][start_col]].is_leaf = False             
               trie_obj.number_of_words -= 1
             else:              
               pass
             words_trie_node = words_trie_node.alpha_map[board[start_row][start_col]]
-            self.helper(board, start_row + 1, start_col, trie_obj, words_trie_node, current_string, result, visited)
-            self.helper(board, start_row, start_col + 1, trie_obj, words_trie_node, current_string, result, visited)
-            self.helper(board, start_row - 1, start_col, trie_obj, words_trie_node, current_string, result, visited)
-            self.helper(board, start_row, start_col - 1, trie_obj, words_trie_node, current_string, result, visited)
-            visited[start_row][start_col] = False                
+            neighbors = [[start_row + 1, start_col], [start_row, start_col + 1], [start_row - 1, start_col], [start_row, start_col - 1]]
+            for neighbor in neighbors:
+                if self.is_valid_neighbor(neighbor, len(board), len(board[0])):
+                  self.helper(board, neighbor[0], neighbor[1], trie_obj, words_trie_node, current_string, result)
+                else:
+                  continue
+            board[start_row][start_col] = t                       
+        else:
+            return                
         return          
-
-
+    
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
         result = set()        
         visited = [[] for i in range(len(board))]
@@ -53,21 +55,14 @@ class Solution:
         n_cols = len(board[0])
         board_size = n_rows * n_cols
         # get unique letters across the board        
-        #board_unique_letters = self._get_unique_letters(board)
-        import time
-        start = time.time()
+        board_unique_letters = self._get_unique_letters(board)
         for word in words:
             if len(word) <= board_size:
                 words_trie.insert(word)
             else:
                 continue                
-        end = time.time()
-        print(f'Trie constructed in {end - start}s')
         current_string = ""
-        start = time.time()
         for row in range(n_rows):
             for col in range(n_cols):
                 self.helper(board, row, col, words_trie, words_trie.trie_root, current_string, result, visited)
-        end = time.time()
-        print(f'DFS done in {end - start}s')
         return list(result)        
