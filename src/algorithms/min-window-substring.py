ce2332bf9d640c2b137b1d0142d737e17c681321
pranav <pranav8iisc@gmail.com>
2023-07-17 18:36:03 +0530

    fix min substring indices

--- src/algorithms/min-window-substring.py
+++ src/algorithms/min-window-substring.py
@@ -5,15 +5,14 @@ class Solution:
             start += 1
         return start            
 
-    def substring_matches_counter(self, substring, counter):
-        #print('Check: ', bool(counter - Counter(substring)))
+    def substring_matches_counter(self, substring, counter):        
         return bool(counter - Counter(substring))
     
-    def update_min_substring(self, start, end, min_start, min_length):
-        if end - start < min_length: 
-            return start, end - start 
+    def update_min_substring(self, min_start, min_end, start, end):
+        if end - start < min_end - min_start: 
+            return start, end
         else: 
-            return min_start, min_length
+            return min_start, min_end
     
     def minWindow(self, s: str, t: str) -> str:
         start = 0
@@ -28,18 +27,18 @@ class Solution:
                 
         t_map = collections.Counter(t) # maintains count of letters in 't'
         min_start = 0
-        min_end = len(t)                        
+        min_end = len(s)                        
         start = self.get_first_matching_letter(start, s, t_map)
         #print('Start-1: ', start)
-        end = start + len(t) - 1
+        end = start + len(t) #- 1
         min_substring = ""
-        while(end < len(s)):                        
+        while(end <= len(s)):                        
             if self.substring_matches_counter(s[start:end], t_map) == False:
                 #print('Matched: ', s[start:end])
                 min_start, min_end = self.update_min_substring(min_start, min_end, start, end)    
                 min_substring = s[min_start:min_end]
-                start = self.get_first_matching_letter(start, s, t_map)
-                end = start + len(t) - 1                    
+                start = self.get_first_matching_letter(start+1, s, t_map)
+                end = start + len(t)
             else:
                 end += 1  
                 
