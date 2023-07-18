f6bee1e8b8052fa57f598ba377e79f5ac07da9ee
pranav <pranav8iisc@gmail.com>
2023-07-18 18:26:28 +0530

    remove redundant end ptr decrement

--- src/algorithms/min-window-substring.py
+++ src/algorithms/min-window-substring.py
@@ -15,8 +15,6 @@ class Solution:
         while(start <= len(s) - 1 and s[start] not in t_map):
             start += 1        
         min_substring = ""
-               
-        #print('Start-1: ', start)
         end = start + len(t) 
         
         min_start = 0
@@ -27,11 +25,7 @@ class Solution:
             c_map = Counter(s[start:end])
         
         while(end <= len(s)):                                                            
-            #print('t map: ', t_map)
-            #print('cmap: ', c_map)
-            #print('start:end map: ', Counter(s[start:end]))
             if (t_map - c_map) == Counter():
-                #print('Matched: ', s[start:end])
                 if min_end - min_start > end - start:
                     min_start, min_end = start, end
                     min_substring = s[min_start:min_end]                
@@ -40,10 +34,6 @@ class Solution:
                 while(start <= len(s) - 1 and s[start] not in t_map):
                     c_map[s[start]] -= 1 # update the counter map to reflect the sliding of substring window.
                     start += 1
-                while(end > start + len(t)):
-                    c_map[s[end]] -= 1
-                    end -= 1                
-                #print('Start end: ', start, end)
             else:                
                 if end < len(s):
                     c_map[s[end]] += 1
