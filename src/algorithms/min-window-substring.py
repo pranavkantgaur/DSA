82a80ad19a155626a14c624f38fb05d5666876dc
pranav <pranav8iisc@gmail.com>
2023-07-18 16:07:25 +0530

    replace with inline compute and avoid counter recreate

--- src/algorithms/min-window-substring.py
+++ src/algorithms/min-window-substring.py
@@ -1,22 +1,7 @@
-# https://leetcode.com/problems/minimum-window-substring/class Solution:
-class Solution:             
-    def get_first_matching_letter(self, start, s, t_map):
-        while(start <= len(s) - 1 and s[start] not in t_map):
-            start += 1
-        return start            
+# https://leetcode.com/problems/minimum-window-substring/
 
-    def substring_matches_counter(self, substring, counter):        
-        return bool(counter - Counter(substring))
-    
-    def update_min_substring(self, min_start, min_end, start, end):
-        if end - start < min_end - min_start: 
-            return start, end
-        else: 
-            return min_start, min_end
-    
+class Solution:                 
     def minWindow(self, s: str, t: str) -> str:
-        start = 0
-        end = len(t)
         if len(t) > len(s):
           return ""        
         if len(s) == 1 and len(t) == 1:
@@ -26,21 +11,45 @@ class Solution:
             return ""                                      
                 
         t_map = collections.Counter(t) # maintains count of letters in 't'
+        start = 0
+        while(start <= len(s) - 1 and s[start] not in t_map):
+            start += 1        
+        min_substring = ""
+               
+        #print('Start-1: ', start)
+        end = start + len(t) 
+        
         min_start = 0
         min_end = len(s)                        
-        start = self.get_first_matching_letter(start, s, t_map)
-        #print('Start-1: ', start)
-        end = start + len(t) #- 1
-        min_substring = ""
-        while(end <= len(s)):                        
-            if self.substring_matches_counter(s[start:end], t_map) == False:
+        
+        c_map = None
+        if end <= len(s):
+            c_map = Counter(s[start:end])
+        
+        while(end <= len(s)):                                                            
+            #print('t map: ', t_map)
+            #print('cmap: ', c_map)
+            #print('start:end map: ', Counter(s[start:end]))
+            if (t_map - c_map) == Counter():
                 #print('Matched: ', s[start:end])
-                min_start, min_end = self.update_min_substring(min_start, min_end, start, end)    
-                min_substring = s[min_start:min_end]
-                start = self.get_first_matching_letter(start+1, s, t_map)
-                end = start + len(t)
-            else:
-                end += 1  
+                if min_end - min_start > end - start:
+                    min_start, min_end = start, end
+                    min_substring = s[min_start:min_end]                
+                c_map[s[start]] -= 1
+                start += 1                
+                while(start <= len(s) - 1 and s[start] not in t_map):
+                    c_map[s[start]] -= 1 # update the counter map to reflect the sliding of substring window.
+                    start += 1
+                while(end > start + len(t)):
+                    c_map[s[end]] -= 1
+                    end -= 1                
+                #print('Start end: ', start, end)
+            else:                
+                if end < len(s):
+                    c_map[s[end]] += 1
+                end += 1                  
+                    
+
                 
         return min_substring                        
             
