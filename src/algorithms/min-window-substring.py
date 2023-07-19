b34707c91851ff05b2cc9f75f5e849ba31a36537
pranav <pranav8iisc@gmail.com>
2023-07-19 11:35:26 +0530

    refactor code and fix incorrect return value

--- src/algorithms/min-window-substring.py
+++ src/algorithms/min-window-substring.py
@@ -1,29 +1,27 @@
 # https://leetcode.com/problems/minimum-window-substring/
 
 class Solution:                 
-    def minWindow(self, s: str, t: str) -> str:
-        if len(t) > len(s):
-          return ""        
-        if len(s) == 1 and len(t) == 1:
-          if s[0] == t[0]:
-            return s[0]
-          else:
-            return ""                                      
-                
+    def minWindow(self, s: str, t: str) -> str:                                               
         t_map = collections.Counter(t) # maintains count of letters in 't'
+        
+        # initialize start to point to first matching letter
         start = 0
         while(start <= len(s) - 1 and s[start] not in t_map):
             start += 1        
-        min_substring = ""
+        # set end to minimum viable value
         end = start + len(t) 
         
+        # track the solution
         min_start = 0
         min_end = len(s)                        
-        
+        min_substring = "" # init output
+
+        # track candidates
         c_map = None
         if end <= len(s):
             c_map = Counter(s[start:end])
-        
+
+        # search for solution among candidates
         while(end <= len(s)):                                                            
             if (t_map - c_map) == Counter():
                 if min_end - min_start > end - start:
@@ -39,9 +37,11 @@ class Solution:
                     c_map[s[end]] += 1
                 end += 1                  
                     
-
-                
-        return s[min_start: min_end]                        
+        # if no candidate was found
+        if min_end - min_start == len(s):
+            return ""                
+        else:
+            return s[min_start: min_end]                        
             
                     
                             
