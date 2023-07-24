2821e7ccde494cd401c413aae4b7acd15e47dce1
pranav <pranav8iisc@gmail.com>
2023-07-24 18:38:24 +0530

    added conditional checks and comments

--- src/algorithms/print_subseuqnces.py
+++ src/algorithms/print_subseuqnces.py
@@ -6,9 +6,12 @@ def get_subsequences(s, current_subs, result):
   if len(s) == 0:
     result.append(current_subs)
   else:    
-    current_subs.append(s[0))
-    get_subsequences(s[1:], current_subs, result)
-    get_subsequences(s[2:], current_subs, result)
+    current_subs.append(s[0])    
+    if len(s) >= 2: # to take
+      get_subsequences(s[1:], current_subs, result)
+      if len(s) >= 3: # not to take
+        get_subsequences(s[2:], current_subs, result)
+    
 if __name__ == '_main_':
   s = 'abeds'
   current_subs = ''
