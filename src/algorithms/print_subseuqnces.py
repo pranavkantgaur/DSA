4f79930eb6d84d1f944eab0d52f988ee0679d189
pranav <pranav8iisc@gmail.com>
2023-07-26 16:02:16 +0530

    fix arguments for recurs. calls

--- src/algorithms/print_subseuqnces.py
+++ src/algorithms/print_subseuqnces.py
@@ -1,21 +1,21 @@
 '''
 https://www.geeksforgeeks.org/print-subsequences-string/
 '''
-
 def get_subsequences(s, current_subs, result):
   if len(s) == 0:
-    result.append(current_subs)
-  else:    
-    current_subs.append(s[0])    
-    if len(s) >= 2: # to take
-      get_subsequences(s[1:], current_subs, result)
-      if len(s) >= 3: # not to take
-        get_subsequences(s[2:], current_subs, result)
+    print(current_subs)
+  else:
+    if len(s) == 1:
+      remaining_str = ''
+    else:
+      remaining_str = s[1:]
+    get_subsequences(remaining_str, current_subs + s[0], result) # take
+    get_subsequences(remaining_str, current_subs, result) # not take   
     
-if __name__ == '_main_':
-  s = 'abeds'
-  current_subs = ''
-  result = []
-  get_subsequences(s, current_subs, result)
-  print(f'Subsequences of {s} are: {result}')
+# Driver code
+# output is set to null before passing in
+# as a parameter
+output = ""
+input = "abcd"
+get_subsequences(input, '', output)
     
