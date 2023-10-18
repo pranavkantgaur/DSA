c0bf63edb7cf1f7f77da11ad8406f1ddda89d013
pranav <pranav8iisc@gmail.com>
2023-10-18 10:59:53 +0530

    simplify processing of slow value

--- src/algorithms/happy_numbers.py
+++ src/algorithms/happy_numbers.py
@@ -1,4 +1,4 @@
-# https://leetcode.com/problems/happy-number/
+# https://leetcode.com/problems/happy-number/ 
 class Solution:
   
   def getNextNum(self, num):
@@ -9,18 +9,12 @@ class Solution:
     return digit_sq_sum
 
   def find(self, num):
-    # TODO: Write your code here
     slow = num
     fast = self.getNextNum(self.getNextNum(num))
-    while(slow != fast):
+
+    while(True):
       slow = self.getNextNum(slow)
       fast = self.getNextNum(self.getNextNum(fast))
       if slow == fast:
-        if slow == 1:
-          return True
-        else:
-          return False               
-    if slow == 1:
-      return True
-    else:
-      return False                
+        break
+    return slow == 1              
