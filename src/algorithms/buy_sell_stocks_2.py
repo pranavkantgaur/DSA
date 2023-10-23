f338944ca6f49f1aac861dac46dc664a3424fa18
pranav <pranav8iisc@gmail.com>
2023-10-23 12:57:00 +0530

    add greedy approach impl

--- src/algorithms/buy_sell_stocks_2.py
+++ src/algorithms/buy_sell_stocks_2.py
@@ -1,1 +1,14 @@
 # https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/description/
+# greedy approach gives o(n), just keep transacting if the next selling price will be higher than current price.
+
+class Solution {
+public:
+    int stockBuyAndSell(int n, int price[]) {
+        int profit = 0;
+        for (int i = 1; i < n; i++) {
+            if (price[i] > price[i - 1])
+                profit += price[i] - price[i - 1];
+        }
+        return profit;
+    }
+};
