616a1d3c2a4e159e15e9d234dbb1795993c210ac
pranav <pranav8iisc@gmail.com>
2023-10-27 14:05:39 +0530

    WIP 2/n: add sliding window impl with hash map based counter

--- src/algorithms/word_concat.py
+++ src/algorithms/word_concat.py
@@ -1,14 +1,32 @@
 # Given a string and a list of words, find all the starting indices of substrings in the given string that are a concatenation of all the given words exactly once without any overlapping of words. It is given that all words are of the same length.
+# approach: sliding window with fixed size and hashmap for word frequency counter
 class Solution:
   def findWordConcatenation(self, str1, words):
     result_indices = []
-    # TODO: Write your code here
+    word_length = len(words[0])
+    left = 0
+    right = 0
+    matched = 0
+    pattern_freq = {}
+    for word in words:
+      if word in pattern_freq:
+        pattern_freq[word] += 1
+      else:
+        pattern_freq[word] = 1
+    while(right < len(str1)):
+      right_word = pattern_freq[right * word_length : (right + 1) * word_length] 
+      left_word = pattern_freq[left * word_length : (left + 1) * word_length]
+      if right_word in pattern_freq:
+        pattern_freq[right_word] -= 1
+        if pattern_freq[right_word] == 0:
+          matched += 1
+      if matched == len(pattern_freq):
+        result_indices.append(left)
+        if pattern_freq[left_word] == 0:
+          matched -= 1
+        pattern_freq[left_word] += 1
+        left += word_length
+      if right_word not in pattern_freq:
+        left += word_length
+      right += word_length
     return result_indices
-    '''
-    1. store hashmap of word freq, for input word list
-    2. run sliding window with incremnets of k, k = len of word
-    3 for each new word between last right and current right pointer, update hashmap by decrementing the counter
-    4. if current word is not in hash counter, update left = current word + 1
-    5. contnue to advance right += 1
-    6. if at any point matched == len of counter, add current left pointer to result list
-    '''
