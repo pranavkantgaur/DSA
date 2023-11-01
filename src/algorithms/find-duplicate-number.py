6d78ba622d2b927471070e1c58b611882df900cd
pranav <pranav8iisc@gmail.com>
2023-11-01 18:37:30 +0530

    add cyclic sort approach

--- src/algorithms/find-duplicate-number.py
+++ src/algorithms/find-duplicate-number.py
@@ -1,27 +1,18 @@
 # https://leetcode.com/problems/find-the-duplicate-number/
 class Solution:
-    def findDuplicate(self, nums: List[int]) -> int:
-        '''
-        uses the input array as both input number list and a hashmap(for marking a number as visited)
-        '''
-        bias = max(nums) + 100
-        n = len(nums)
-        # detect duplicate
-        for num in nums:
-            if num > n: # this position has been visited.
-                num_index = num - bias - 1
-            else:
-                num_index = num - 1
-            if nums[num_index] > n: # already visited once
-                duplicate = num_index + 1
-                break
-            else: # not visited yet
-                nums[num_index] += bias # mark as visited
-            
-        # restore the array
-        for num in nums:
-            if num > n:
-                num -= bias
-        return duplicate 
+  def findNumber(self, nums):
+    current = 0
+    while(current < len(nums)):
+      if nums[current] != current + 1:
+        t = nums[nums[current] - 1]
+        if t == nums[current]: # duplicate
+          return nums[current]
+        else: # swap
+          nums[nums[current] - 1] = nums[current]          
+          nums[current] = t
+      else: # number already at right place
+        current += 1          
+    return -1
+
                         
         
