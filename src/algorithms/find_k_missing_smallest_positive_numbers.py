dc40eb0deb00f5fc8ebdd96a5fbef7e49c2ad1a2
pranav <pranav8iisc@gmail.com>
2023-11-02 12:21:45 +0530

    add fix for missing out on n > len(nums) as part of soln

--- src/algorithms/find_k_missing_smallest_positive_numbers.py
+++ src/algorithms/find_k_missing_smallest_positive_numbers.py
@@ -13,17 +13,28 @@ class Solution:
           if t != nums[current]:
             nums[nums[current] - 1] = nums[current]
             nums[current] = t
-          else: # duplicate
+          else:
             current += 1
       else:
         current += 1
     
-    print('arr: ', nums)
     # search for k missing +ve numbers
     current = 0
-    while(k > 0 and current < len(nums)):
+    extraNums = set() # store numbers which are + and > len(nums), so as to avoid them being added as missing numbers later.
+    while(current < len(nums) and k > 0):
       if nums[current] != current + 1:
         missingNumbers.append(current + 1)
+        if nums[current] > len(nums):
+          extraNums.add(nums[current])
         k -= 1
       current += 1
+
+    missing_num_cand = len(nums) + 1
+    while(k > 0):
+      if missing_num_cand in extraNums:
+        missing_num_cand += 1
+      else:
+        missingNumbers.append(missing_num_cand)
+        k -= 1
+
     return missingNumbers
