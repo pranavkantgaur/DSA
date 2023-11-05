eb9744aabd249a9b2cfefb63c14a4603df76e1e4
pranav <pranav8iisc@gmail.com>
2023-11-05 08:05:44 +0530

    add bsearch based soln

---
+++ src/algorithms/min_diff_element.py
@@ -0,0 +1,17 @@
+# Given an array of numbers sorted in ascending order, find the element in the array that has the minimum difference with the given ‘key’.
+class Solution:
+  def searchMinDiffElement(self, arr, key):
+    left = 0
+    right = len(arr) - 1
+    while(left <= right):
+      mid = left + (right - left) // 2
+      if arr[mid] > key:
+        right = mid - 1
+      elif arr[mid] < key:
+        left = mid + 1
+      else:
+        return arr[mid]
+    if right == -1 or abs(arr[left] - key) < abs(arr[right] - key):
+      return arr[left]
+    else:
+      return arr[right]
