13f7597457a2e9d885ad65e60befca4f6c92c4b6
pranav <pranav8iisc@gmail.com>
2023-11-06 14:40:14 +0530

    remove hashmap usage

--- src/algorithms/daily_tempratures.py
+++ src/algorithms/daily_tempratures.py
@@ -1,26 +1,13 @@
 # Given an array of integers temperatures representing daily temperatures, your task is to calculate how many days you have to wait until a warmer temperature. If there is no future day for which this is possible, put 0 instead.
+
 class Solution:
     def dailyTemperatures(self, temperatures):
-        # using monotonically decreasing stack and a hashmap of list
-        # hashmap of list contains the relative index of next higher temprature day. Since there can be multiple days with same temprature, we use a list for each key temprature.
-        mon_dec_temp_stack = []
-        nge_id_hmap = {}
-        for temp_id in range(len(temperatures)):
-          while(len(mon_dec_temp_stack) > 0 and mon_dec_temp_stack[-1][0] < temperatures[temp_id]):
-            top, id = mon_dec_temp_stack.pop(-1)
-            if top not in nge_id_hmap:
-              nge_id_hmap[top] = []
-            nge_id_hmap[top].append(temp_id - id)
-            
-          mon_dec_temp_stack.append([temperatures[temp_id], temp_id])  
-        while(len(mon_dec_temp_stack) > 0):
-          top, id = mon_dec_temp_stack.pop(-1)
-          if top not in nge_id_hmap:
-            nge_id_hmap[top] = []
-          nge_id_hmap[top].append(0)
-
-        next_higher_temp_day = []
-        for temp in temperatures:
-          next_higher_temp_day.append(nge_id_hmap[temp].pop(0))
-        
+        # use monotonically decreasing stack and store the ids of days instead of corresponding tempratures.
+        next_higher_temp_day = [0 for _ in range(len(temperatures))]
+        stack = []
+        for id in range(len(temperatures)):
+          while(len(stack) > 0  and temperatures[id] > temperatures[stack[-1]]):
+            prev_day_id = stack.pop(-1)
+            next_higher_temp_day[prev_day_id] = id - prev_day_id        
+          stack.append(id)
         return next_higher_temp_day
