b7879658dd22c78c1de76a257d44bdd1bafe6f30
pranav <pranav8iisc@gmail.com>
2023-11-07 07:40:50 +0530

    add bsearch based solution space search

--- algorithms/split_array_largest_sum.py
+++ algorithms/split_array_largest_sum.py
@@ -1,16 +1,25 @@
 # https://leetcode.com/problems/split-array-largest-sum/
-class Solution:
-    def isPartitionPossible(self, target_sum, arr, k):
-        # check if it is possible to partition the array into k partitions with max sum of subarray being atmost k
-        return is_possible
+class Solution:    
+    def predicate(self, target, k, nums):
+        total = 0
+        parts = 1
+        for num in nums:
+            total += num
+            if total > target:
+                total = num
+                parts += 1
+                if parts > k:
+                    return False
 
+        return True
     def splitArray(self, nums: List[int], k: int) -> int:
         left = max(nums)
         right = sum(nums)
-        while(left <= right):
+        while(left < right):
             mid = left + (right - left) // 2
-            if self.isPartitionPossible(mid, nums, k)
-                right = mid - 1
-        return left
-
+            if self.predicate(mid, k, nums):
+                high = mid
+            else:
+                left = mid + 1
+        return left        
         
