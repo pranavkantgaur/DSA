5290028a74d664edd63f67777b4a84f50d1e5d25
pranav <pranav8iisc@gmail.com>
2023-11-11 20:01:30 +0530

    WIP: add approach comments: dfs with diagonal distance based clustering

---
+++ src/algorithms/diagonal_view_of_binary_tree.py
@@ -0,0 +1,130 @@
+# practice.geeksforgeeks.org/problems/diagonal-traversal-of-binary-tree/
+#User function Template for python3
+
+'''
+# Node Class:
+class Node:
+    def _init_(self,val):
+        self.data = val
+        self.left = None
+        self.right = None
+'''
+#Complete the function below
+class Solution:
+    def diagonal(self,root):
+        #:param root: root of the given tree.
+        #return: print out the diagonal traversal,  no need to print new line
+        #code here
+        '''
+        1. Do DFS and assign every right node as the same dia. group as the parent node
+        2. To every node on the left edge of a parent node assign dia. group as parent + 1
+        3. store all these nodes clustered by diagonal group in a hashmap of node-lists
+        4. print the lists by increasing dia. group distance.
+        '''
+        stack = []
+        stack.append([root, 0])
+        while(len(stack)):
+            node, dist = stack.pop(-1)
+            diag_map[dist].append(node)
+            if node.left:
+                stack.append([node.left, dist])
+            if node.left:
+                stack.append([node.right, dist + 1])
+
+
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import sys
+sys.setrecursionlimit(50000)
+#Contributed by Sudarshan Sharma
+from collections import deque
+# Tree Node
+class Node:
+    def __init__(self, val):
+        self.right = None
+        self.data = val
+        self.left = None
+
+'''
+class Node:
+    def __init__(self, val):
+        self.right = None
+        self.data = val
+        self.left = None
+'''
+
+# Function to Build Tree   
+def buildTree(s):
+    #Corner Case
+    if(len(s)==0 or s[0]=="N"):           
+        return None
+        
+    # Creating list of strings from input 
+    # string after spliting by space
+    ip=list(map(str,s.split()))
+    
+    # Create the root of the tree
+    root=Node(int(ip[0]))                     
+    size=0
+    q=deque()
+    
+    # Push the root to the queue
+    q.append(root)                            
+    size=size+1 
+    
+    # Starting from the second element
+    i=1                                       
+    while(size>0 and i<len(ip)):
+        # Get and remove the front of the queue
+        currNode=q[0]
+        q.popleft()
+        size=size-1
+        
+        # Get the current node's value from the string
+        currVal=ip[i]
+        
+        # If the left child is not null
+        if(currVal!="N"):
+            
+            # Create the left child for the current node
+            currNode.left=Node(int(currVal))
+            
+            # Push it to the queue
+            q.append(currNode.left)
+            size=size+1
+        # For the right child
+        i=i+1
+        if(i>=len(ip)):
+            break
+        currVal=ip[i]
+        
+        # If the right child is not null
+        if(currVal!="N"):
+            
+            # Create the right child for the current node
+            currNode.right=Node(int(currVal))
+            
+            # Push it to the queue
+            q.append(currNode.right)
+            size=size+1
+        i=i+1
+    return root
+    
+    
+if __name__=="__main__":
+    t=int(input())
+    for _ in range(0,t):
+        s=input()
+        root=buildTree(s)
+        obj=Solution()
+        diagonalNode = obj.diagonal(root)
+        for node in diagonalNode:
+            print(node,end=' ')
+        print()
+
+# } Driver Code Ends
