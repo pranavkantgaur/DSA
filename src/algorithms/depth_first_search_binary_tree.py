00895460b873fdaddc1f2db1c8e46d0418d64ca3
pranav <pranav8iisc@gmail.com>
2023-11-14 11:29:49 +0530

    add result array

--- src/algorithms/depth_first_search_binary_tree.py
+++ src/algorithms/depth_first_search_binary_tree.py
@@ -1,16 +1,16 @@
 # implement dfs using stack
-def dfs_using_stack(root):
+def dfs(root):
   stack = []
-  stack.append([root, 0]) # node, visited?
+  stack.append([root, 0])
+  result = []
   while(len(stack)):
-    node, visited = stack[-1] # peek
-    print(node.data)
-    if visited:
-      if node.left:
-        stack.append([node.left, 0])
-      stack.pop(-1)
-    else:
-      stack[-1][1] = 1 # mark visited
-      if node.right:
-        stack.append([node.right, 0])
-  return        
+	  node, visited = stack[-1]	
+	  if visited:
+		  if node.left:
+			  stack.append([node.left, 0])
+		  stack.pop(-1)
+	  else:
+		  result.append(node.data) # [1, 4 ,5, 2, 3]
+		  stack[-1][1] = 1 # mark as visited
+		  if node.right:
+			  stack.append([node.right, 0])  
