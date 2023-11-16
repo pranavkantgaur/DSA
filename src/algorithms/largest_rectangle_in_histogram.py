3eb2903ba51a38a975508ba94030868c98f05b4d
pranav <pranav8iisc@gmail.com>
2023-11-16 15:50:58 +0530

    replace soln with single stack and remove hashmap

--- src/algorithms/largest_rectangle_in_histogram.py
+++ src/algorithms/largest_rectangle_in_histogram.py
@@ -1,27 +1,24 @@
 # https://leetcode.com/problems/largest-rectangle-in-histogram/
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
-        max_area = 0
         stack = []
-        hMap_lr = {}
-        hMap_rl = {}
+        max_area = 0
         for id in range(len(heights)):
-            while(len(stack) and heights[stack[-1]] > heights[id]):
-                hMap_lr[stack.pop(-1)] = id
+            while(len(stack) and heights[id] < heights[stack[-1]]):
+                top_id = stack.pop()
+                if len(stack):
+                    area = heights[top_id] * (id - stack[-1] - 1)
+                else:
+                    area = heights[top_id] * id
+                if max_area < area:
+                    max_area = area
             stack.append(id)
-        while(len(stack)):
-            hMap_lr[stack.pop(-1)] = len(heights)
-
-        # right to left
-        for id in range(len(heights) - 1, -1, -1):
-            while(len(stack) and heights[stack[-1]] > heights[id]):
-                hMap_rl[stack.pop(-1)] = id
-            stack.append(id)
-        while(len(stack)):
-            hMap_rl[stack.pop(-1)] = -1
-            
-        for id in range(len(heights)):
-            area = heights[id] * (hMap_lr[id] - hMap_rl[id] - 1)
-            if area > max_area:
+        while(len(stack)):            
+            top_id = stack.pop()
+            if len(stack):
+                area = heights[top_id] * (len(heights) - stack[-1] - 1)
+            else:
+                area = heights[top_id] * (len(heights))
+            if max_area < area:
                 max_area = area
         return max_area
