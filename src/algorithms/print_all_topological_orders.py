27781f727691ce7433011f89fdd87027179e02f9
pranav <pranav8iisc@gmail.com>
2023-12-13 14:34:44 +0530

    fix creating sources list for downstream topo order generation

--- src/algorithms/print_all_topological_orders.py
+++ src/algorithms/print_all_topological_orders.py
@@ -8,21 +8,29 @@ class Solution:
 
   def print_topo_orders(self, graph, indeg, sources, sortedOrder):
     if len(sources):
-      for vertex in sources:
-        source = sources.pop(vertex)
+      for source in sources:
         sortedOrder.append(source)
         sources_downtream = sources.copy()
-        sources.append(source)
-        for deps in graph[source]:
-          indeg[deps] -= 1
-          if indeg[deps] == 0:
-            sources_downtream.append(deps)
-        self.print_topo_orders(graph, indeg, sources_downtream, sortedOrder)
+        sources_downtream.remove(source)        
+        for dep in graph[source]:
+          indeg[dep] -= 1
+          if indeg[dep] == 0:
+            sources_downtream.append(dep)
+        self.print_topo_orders(graph, indeg, sources_downtream, sortedOrder)        
+        for dep in graph[source]:
+          indeg[dep] += 1
+        sortedOrder.pop(-1)
+        
     else:
       if len(sortedOrder) == len(indeg):
-        self.orders.append(sortedOrder)
+        self.orders.append(sortedOrder.copy())
+        print(sortedOrder)
+        print('res: ', self.orders)
+        
   
   def printOrders(self, tasks, prerequisites):
+    if tasks == 0:
+      return []
     # subdivide using recursion when multiple sources are detected in current graph
     graph = {i: [] for i in range(tasks)}
     indeg = {i:0 for i in range(tasks)}
@@ -31,5 +39,6 @@ class Solution:
       indeg[prerequisite[1]] += 1
     sources = [i for i in range(tasks) if indeg[i] == 0]
     sortedOrder = []
-    self.print_topo_orders(graph, indeg, source, sortedOrder)
+    self.print_topo_orders(graph, indeg, sources, sortedOrder)
+    #print('check: ', self.orders)
     return self.orders
