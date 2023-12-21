1303ab92cc6fa6d6a87bd4c1b60d55ea23313bbf
pranav <pranav8iisc@gmail.com>
2023-12-21 13:24:28 +0530

    fix multiple insertions of a cell in dfs stack

--- src/algorithms/compute_perimeter_of_lake.py
+++ src/algorithms/compute_perimeter_of_lake.py
@@ -7,33 +7,35 @@
 # The given matrix has only one island, write a function to find the perimeter of that island.
 
 class Solution:
-  
   def dfs(self, row, col, matrix, param_length):
     stack = [[row, col]]
     while(len(stack)):
-      top_row, top_col = stack.pop()
-      matrix[top_row][top_col] = 2
+      top_row, top_col = stack.pop()      
       # check neighbors
       if top_row == 0 or matrix[top_row - 1][top_col] == 0:
         param_length += 1
       else:
         if matrix[top_row - 1][top_col] == 1:
           stack.append([top_row - 1, top_col])
+          matrix[top_row - 1][top_col] = 2
       if top_row == len(matrix) - 1 or matrix[top_row + 1][top_col] == 0:
         param_length += 1
       else:
         if matrix[top_row + 1][top_col] == 1:
           stack.append([top_row + 1, top_col])        
+          matrix[top_row + 1][top_col] = 2
       if top_col == 0 or matrix[top_row][top_col - 1] == 0:
         param_length += 1
       else:
         if matrix[top_row][top_col - 1] == 1:
           stack.append([top_row, top_col - 1])      
+          matrix[top_row][top_col - 1] = 2
       if top_col == len(matrix[0]) - 1 or matrix[top_row][top_col + 1] == 0:
         param_length += 1
       else:
         if matrix[top_row][top_col + 1] == 1:
           stack.append([top_row, top_col + 1])        
+          matrix[top_row][top_col + 1] = 2
     return param_length
   
   def findIslandPerimeter(self, matrix):
@@ -41,6 +43,7 @@ class Solution:
     for row in range(len(matrix)):
       for col in range(len(matrix[0])):
         if matrix[row][col]:
+          matrix[row][col] = 2
           param_length = self.dfs(row, col, matrix, param_length)
           return param_length
     return param_length
