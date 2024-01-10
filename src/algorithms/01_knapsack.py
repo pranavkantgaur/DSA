7141acffeaeadd011b282e122ff853eb10b0dc71
pranav <pranav8iisc@gmail.com>
2024-01-10 13:03:27 +0530

    add space-optimized implementation

--- src/algorithms/01_knapsack.py
+++ src/algorithms/01_knapsack.py
@@ -3,18 +3,18 @@ class Solution:
   def solveKnapsack(self, profits, weights, capacity):
     if len(weights) == 0 or capacity == 0:
       return 0
-    dp = [[-1 for _ in range(capacity + 1)] for _ in range(len(weights) + 1)]        
+    dp = [-1 for _ in range(capacity + 1)]
     for index in range(len(weights), 0, -1):
-      for cap in range(capacity + 1):
-        if index == len(weights): # last position in input array, decision-making is purely based on the direct comparison between current capacity and last item weight
-          if cap >= weights[index - 1]:
-            dp[index][cap] = profits[index - 1]
-          else:
-            dp[index][cap] = 0
-        else:        
+      for cap in range(capacity, 0, -1): # iterating right-to-left, so that we do not access subproblem soln for the current index during include profit calculation
+        if index == len(weights):
+            if weights[index - 1] <= cap:
+              dp[cap] = profits[index - 1]
+            else:
+              dp[cap] = 0
+        else:
           include_profit = 0
-          if cap >= weights[index - 1]:
-            include_profit = profits[index - 1] + dp[index + 1][cap - weights[index - 1]]             
-          exclude_profit = dp[index + 1][cap]
-          dp[index][cap] = max(include_profit, exclude_profit)
-    return dp[1][capacity]    
+          if weights[index - 1] <= cap:
+            include_profit = profits[index - 1] + dp[cap - weights[index - 1]]
+          exclude_profit = dp[cap]
+          dp[cap] = max(include_profit, exclude_profit)
+    return dp[capacity]  
