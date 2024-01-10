7ded9f9ae821d453ecdec6b98641094aa4ba8204
pranav <pranav8iisc@gmail.com>
2024-01-10 12:29:39 +0530

    add logic for invalid input edge-case

--- src/algorithms/01_knapsack.py
+++ src/algorithms/01_knapsack.py
@@ -1,6 +1,8 @@
 # Given two integer arrays to represent weights and profits of ‘N’ items, we need to find a subset of these items which will give us maximum profit such that their cumulative weight is not more than a given number ‘C.’ Each item can only be selected once, which means either we put an item in the knapsack or we skip it.
 class Solution:
   def solveKnapsack(self, profits, weights, capacity):
+    if len(weights) == 0 or capacity == 0:
+      return 0
     dp = [[-1 for _ in range(capacity + 1)] for _ in range(len(weights) + 1)]        
     for index in range(len(weights), 0, -1):
       for cap in range(capacity + 1):
