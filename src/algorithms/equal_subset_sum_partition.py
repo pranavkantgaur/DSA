7d23c527c42e9e3926f5d52b0d02eeb2e12a42fd
pranav <pranav8iisc@gmail.com>
2024-01-14 14:27:00 +0530

    WIP 1/n: add top-down memoization solution

--- src/algorithms/equal_subset_sum_partition.py
+++ src/algorithms/equal_subset_sum_partition.py
@@ -1,16 +1,32 @@
 # Given a set of positive numbers, find if we can partition it into two subsets such that the sum of elements in both subsets is equal.
-def helper(self, arr, start_id, k, curr_sum, total_sum):
-    if curr_sum == total_sum - curr_sum: return True
-    if start_id == len(arr) or len(arr) - start_id < k: return False
-    include = self.helper(arr, start_id + 1, k - 1, curr_sum + arr[start_id], total_sum)
+class Solution:
+  
+  '''
+  How to create a dp array for this? what is the state of the problem?
+  start id, length of subarray, 
+  '''
+
+  def findKSubset(self, arr, start_id, k, curr_sum, total_sum, dp):
+    if curr_sum == total_sum - curr_sum: 
+      dp[start_id][k] = 1
+      return True
+    if start_id == len(arr) or len(arr) - start_id < k: 
+      dp[start_id][k] = 0
+      return False
+    if dp[start_id + 1][k - 1] == -1:
+      self.findKSubset(arr, start_id + 1, k - 1, curr_sum + arr[start_id], total_sum, dp)
+    include = dp[start_id + 1][k - 1]
     if not include:
-        exclude = self.helper(arr, start_id + 1, k, curr_sum, total_sum)
+      if dp[start_id + 1][k] == -1:
+        self.findKSubset(arr, start_id + 1, k, curr_sum, total_sum, dp)
+    exclude = dp[start_id + 1][k]
     return include or exclude
 
-def main(arr):
+  def canPartition(self, arr):
     total_sum = sum(arr)
-    for k in range(1, len(arr) // 2):
-        for start_id in range(len(arr)):
-            curr_sum = 0
-            if self.helper(arr, start_id, k, curr_sum, total_sum): return True # given the subarray starting from start_id, find if we have a subset of size k for which 2 subset equality is satisfied?
+    dp = [[[-1] for _ in range(len(arr) + 1)] for _ in range(len(arr) + 1)]
+    for k in range(1, len(arr) // 2 + 1):
+      for start_id in range(len(arr) - k + 1):
+        curr_sum = 0
+        if self.findKSubset(arr, start_id, k, curr_sum, total_sum, dp): return True # given the subarray starting from start_id, find if we have a subset of size k for which 2 subset equality is satisfied?
     return False
