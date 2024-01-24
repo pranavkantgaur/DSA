2d08af2d0d649cf5af65c75df98eff7224b879ce
pranav <pranav8iisc@gmail.com>
2024-01-24 19:09:02 +0530

    WIP 1/n: add bottom up approach implementation

--- src/algorithms/equal_subset_sum_partition.py
+++ src/algorithms/equal_subset_sum_partition.py
@@ -1,29 +1,15 @@
 # Given a set of positive numbers, find if we can partition it into two subsets such that the sum of elements in both subsets is equal.
-class Solution:
-  
-  '''
-  How to create a dp array for this? what is the state of the problem?
-  start id, length of subarray, 
-  '''
-
-  # top-down memoization implementation.
-  def helper(self, arr, start_id, current_sum, total_sum, dp):
-    if dp[start_id][current_sum] != -1: return dp[start_id][current_sum]
-    if total_sum == 2 * current_sum: 
-      dp[start_id][current_sum] = 1
-      return 1
-    if start_id == len(arr):
-      dp[start_id][current_sum] = 0
-      return 0
-    include = self.helper(arr, start_id + 1, current_sum + arr[start_id], total_sum, dp)
-    exclude = self.helper(arr, start_id + 1, current_sum, total_sum, dp)
-    dp[start_id][current_sum] = include or exclude
-    return dp[start_id][current_sum]
-
+class Solution: 
   def canPartition(self, arr):
-    dp = [[-1 for _ in range(sum(arr) + 1)] for _ in range(len(arr) + 1)]
-    can_partition = self.helper(arr, 0, 0, sum(arr), dp)
-    if can_partition: 
-      return True
-    else: 
-      return False
+    total_sum = sum(arr)
+    dp = [[-1 for _ in range(total_sum + 1)] for _ in range(len(arr) + 1)]
+    for id in range(len(arr) , -1, -1):
+      for current_sum in range(0, total_sum + 1):
+        if id == len(arr):
+          if current_sum * 2 == total_sum:
+            dp[id][current_sum] = 1
+          else:
+            dp[id][current_sum] = 0
+        else:
+          dp[id][current_sum] = dp[id + 1][current_sum + arr[id]] or dp[id + 1][current_sum]
+    return dp[0][0]
