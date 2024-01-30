ffdcec853955671d4bde4404fc56bdc4c7424562
pranav <pranav8iisc@gmail.com>
2024-01-30 11:35:53 +0530

    fixes out of bound access along dp array columns

--- src/algorithms/equal_subset_sum_partition.py
+++ src/algorithms/equal_subset_sum_partition.py
@@ -4,7 +4,8 @@ class Solution:
     total_sum = sum(arr)
     dp = [[-1 for _ in range(total_sum + 1)] for _ in range(len(arr) + 1)]
     for id in range(len(arr) , -1, -1):
-      for current_sum in range(0, total_sum + 1):
+      right_sum = sum(arr[id:])
+      for current_sum in range(0, total_sum - right_sum + 1):
         if id == len(arr):
           if current_sum * 2 == total_sum:
             dp[id][current_sum] = 1
@@ -12,4 +13,4 @@ class Solution:
             dp[id][current_sum] = 0
         else:
           dp[id][current_sum] = dp[id + 1][current_sum + arr[id]] or dp[id + 1][current_sum]
-    return dp[0][0]
+    return dp[0][0] == 1
