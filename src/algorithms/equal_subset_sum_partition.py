11522926010d349c5aef7e28a414de10236411e3
pranav <pranav8iisc@gmail.com>
2024-01-30 11:54:07 +0530

    add space optimized implementation of bottom up approach

--- src/algorithms/equal_subset_sum_partition.py
+++ src/algorithms/equal_subset_sum_partition.py
@@ -2,15 +2,15 @@
 class Solution: 
   def canPartition(self, arr):
     total_sum = sum(arr)
-    dp = [[-1 for _ in range(total_sum + 1)] for _ in range(len(arr) + 1)]
+    dp = [-1 for _ in range(total_sum + 1)]
     for id in range(len(arr) , -1, -1):
       right_sum = sum(arr[id:])
       for current_sum in range(0, total_sum - right_sum + 1):
         if id == len(arr):
           if current_sum * 2 == total_sum:
-            dp[id][current_sum] = 1
+            dp[current_sum] = 1
           else:
-            dp[id][current_sum] = 0
+            dp[current_sum] = 0
         else:
-          dp[id][current_sum] = dp[id + 1][current_sum + arr[id]] or dp[id + 1][current_sum]
-    return dp[0][0] == 1
+          dp[current_sum] = dp[current_sum + arr[id]] or dp[current_sum]
+    return dp[0] == 1
