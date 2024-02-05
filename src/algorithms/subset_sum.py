4c501e9e1995733e9a0424cb8828c20f4d797480
pranav <pranav8iisc@gmail.com>
2024-02-05 12:04:14 +0530

    add brute force soln

--- src/algorithms/subset_sum.py
+++ src/algorithms/subset_sum.py
@@ -1,22 +1,12 @@
 # Given a set of positive numbers, determine if a subset exists whose sum is equal to a given number ‘S’.
-class Solution:
-  def helper(self, start_id, target_sum, dp):
-    if target_sum == 0: 
-      dp[start_id][target_sum] = 1
-      return 1
-    if target_sum < 0:
-      return 0
-    if dp[start_id][target_sum] == -1:
-      if target_sum - arr[start_id] >= 0 and dp[start_id + 1][target_sum - arr[start_id]] != -1:
-        include = dp[start_id + 1][target_sum - arr[start_id]]
-      else:
-        include = self.helper(start_id + 1, target_sum - arr[start_id], dp)
-      exclude = dp[start_id + 1][target_sum]
-      if include == 0 and dp[start_id + 1][target_sum] == -1:
-        exclude = self.helper(start_id + 1, target_sum, dp)
-      dp[start_id][target_sum] = include or exclude
-    return dp[start_id][target_sum]  
+class Solution:  
+  def helper(self, start_id, arr, target_sum):    
+    if target_sum == 0: return True
+    if target_sum < 0: return False
+    if start_id == len(arr): return False
+    include = self.helper(start_id + 1, arr, target_sum - arr[start_id])
+    exclude = self.helper(start_id + 1, arr, target_sum)
+    return include or exclude
+  
   def canPartition(self, num, sum):
-    dp = [[-1 for _ in range(sum + 1)] for _ in range(len(num) + 1)]
-    self.helper(0, 0, dp)
-    return dp[0][0] == 1
+    return self.helper(0, num, sum)
