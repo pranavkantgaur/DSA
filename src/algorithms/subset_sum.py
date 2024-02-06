7872af3cc6541089d2f2cd550e03c0844116cb56
pranav <pranav8iisc@gmail.com>
2024-02-06 12:40:02 +0530

    fix indices

--- src/algorithms/subset_sum.py
+++ src/algorithms/subset_sum.py
@@ -2,12 +2,12 @@
 class Solution:   
   def canPartition(self, num, sum):
     dp = [[-1 for _ in range(sum + 1)] for _ in range(len(num) + 1)]
-    for start_id in range(len(num), -1, -1):
+    for start_id in range(len(num), 0, -1):
       for target_sum in range(sum + 1):
         if target_sum == 0:
           dp[start_id][target_sum] = 1
           continue        
-        if start_id == len(num):
+        if start_id == len(num):          
           if target_sum - num[start_id - 1] == 0: 
               dp[start_id][target_sum] = 1
           else:
@@ -22,4 +22,4 @@ class Solution:
         if include == 0:
           exclude = dp[start_id + 1][target_sum]
         dp[start_id][target_sum] = include or exclude
-    return dp[0][sum] == 1
+    return dp[1][sum] == 1
