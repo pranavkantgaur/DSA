a8c6e5f69358ea2797982dae2c34da947f7b2fc0
pranav <pranav8iisc@gmail.com>
2024-02-14 12:11:28 +0530

    WIP 2/n: add index fixes

--- src/min_subset_sum_difference.py
+++ src/min_subset_sum_difference.py
@@ -1,26 +1,29 @@
 # Given a set of positive numbers, partition the set into two subsets with minimum difference between their subset sums.
-def helper(self, start_id, current_sum, min_diff, dp):
-    if start_id < len(arr):
-        if dp[start_id][current_sum] != -1: 
-            return
-        else:
-            if start_id == len(arr) - 1:
-                include_diff = abs(sum(arr) - 2 * current_sum + arr[start_id])
-                exclude_diff = abs(sum(arr) - 2 * current_sum)
-                dp[start_id][current_sum] = min(min_diff, include_diff, exclude_diff)
-            else:
-                if dp[start_id + 1][current_sum + arr[start_id]] == -1:
-                    self.helper(start_id + 1, current_sum + arr[start_id], min_diff, dp)
-                include_diff = dp[start_id + 1][current_sum + arr[start_id]]
-                if dp[start_id + 1][current_sum] == -1:
-                    self.helper(start_id + 1, current_sum, include_diff, dp)
-                exclude_diff = dp[start_id + 1][current_sum]
-                dp[start_id][current_sum] = exclude_diff
-           return
-    else:
+class Solution:
+  
+  def helper(self, start_id, current_sum, min_diff, arr, dp):
+    if start_id <= len(arr): # valid state?
+      if dp[start_id][current_sum] != -1: # already solved?
         return
+      else:
+        if start_id == len(arr): # base case?
+          include_diff = abs(sum(arr) - 2 * current_sum + arr[start_id - 1])
+          exclude_diff = abs(sum(arr) - 2 * current_sum)
+          dp[start_id][current_sum] = min(min_diff, include_diff, exclude_diff)
+        else:
+          if dp[start_id + 1][current_sum + arr[start_id - 1]] == -1: # include problem solved?
+            self.helper(start_id + 1, current_sum + arr[start_id - 1], min_diff, arr, dp)
+          include_diff = dp[start_id + 1][current_sum + arr[start_id - 1]]
+          if dp[start_id + 1][current_sum] == -1: # exclude problem solved?
+            self.helper(start_id + 1, current_sum, include_diff, arr, dp)
+          exclude_diff = dp[start_id + 1][current_sum]
+          dp[start_id][current_sum] = exclude_diff
+        return
+    else:
+      return
 
-def main(arr):
-    dp = [[-1 for _ in range(sum(arr) + 1)] for _ in range(len(arr) + 1)]
-    self.helper(0, 0, sum(arr), dp)
-    return dp[0][0]
+  def canPartition(self, num):
+    dp = [[-1 for _ in range(sum(num) + 1)] for _ in range(len(num) + 1)]
+    self.helper(1, 0, sum(num), num, dp)
+    return dp[1][0]
+    
