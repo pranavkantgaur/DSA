3e5ac865cbbcb79fe903dddf4f8c68d0c362e512
pranav <pranav8iisc@gmail.com>
2024-02-27 12:55:45 +0530

    add space optimized soln

--- src/min_subset_sum_difference.py
+++ src/min_subset_sum_difference.py
@@ -1,29 +1,16 @@
 # Given a set of positive numbers, partition the set into two subsets with minimum difference between their subset sums.
-class Solution:
-  
-  def helper(self, start_id, current_sum, min_diff, arr, dp):
-    if start_id <= len(arr): # valid state?
-      if dp[start_id][current_sum] != -1: # already solved?
-        return
-      else:
-        if start_id == len(arr): # base case?
-          include_diff = abs(sum(arr) - 2 * current_sum + arr[start_id - 1])
-          exclude_diff = abs(sum(arr) - 2 * current_sum)
-          dp[start_id][current_sum] = min(min_diff, include_diff, exclude_diff)
-        else:
-          if dp[start_id + 1][current_sum + arr[start_id - 1]] == -1: # include problem solved?
-            self.helper(start_id + 1, current_sum + arr[start_id - 1], min_diff, arr, dp)
-          include_diff = dp[start_id + 1][current_sum + arr[start_id - 1]]
-          if dp[start_id + 1][current_sum] == -1: # exclude problem solved?
-            self.helper(start_id + 1, current_sum, include_diff, arr, dp)
-          exclude_diff = dp[start_id + 1][current_sum]
-          dp[start_id][current_sum] = exclude_diff
-        return
-    else:
-      return
-
+class Solution:  
   def canPartition(self, num):
-    dp = [[-1 for _ in range(sum(num) + 1)] for _ in range(len(num) + 1)]
-    self.helper(1, 0, sum(num), num, dp)
-    return dp[1][0]
-    
+    if len(num) == 1: return num[0]
+    dp = [-1 for _ in range(sum(num) + 1)]
+    for start_id in range(len(num), 0, -1):
+      current_sum_range = sum(num[:start_id - 1])
+      for current_sum in range(current_sum_range):
+        if start_id == len(num):
+          include_diff = abs(sum(num) - 2 * (current_sum + num[start_id - 1]))
+          exclude_diff = abs(sum(num) - 2 * current_sum)
+        else:
+            include_diff = dp[current_sum + num[start_id - 1]]
+            exclude_diff = dp[current_sum]
+        dp[current_sum] = min(include_diff, exclude_diff)
+    return dp[0]
