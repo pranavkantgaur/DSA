c17da7c2bbe3ed5739a87b3e71b160ff52c8d320
pranav <pranav8iisc@gmail.com>
2024-04-08 06:21:16 +0530

    add space optimised bottom up soln

--- src/algorithms/subsets_with_a_sum.py
+++ src/algorithms/subsets_with_a_sum.py
@@ -1,21 +1,21 @@
 class Solution:  
   def countSubsets(self, num, sum1):
     #return self.helper(0, sum1, 0, num) # [1, 1, 2, 3], 4 => 3
-    dp = [[-1 for _ in range(sum1 + 1)] for _ in range(len(num) + 1)]
+    dp = [-1 for _ in range(sum1 + 1)]
     for start_id in range(len(num), 0, -1):
-      for target_sum in range(sum1 + 1):
+      for target_sum in range(sum1, -1, -1):
         if target_sum == 0:
-          dp[start_id][0] = 1
+          dp[0] = 1
           continue
         if start_id == len(num):
           if target_sum - num[start_id - 1] == 0:
-            dp[start_id][target_sum] = 1
+            dp[target_sum] = 1
           else:
-            dp[start_id][target_sum] = 0
+            dp[target_sum] = 0
         else:
           if target_sum - num[start_id - 1] >= 0:
-            dp[start_id][target_sum] = dp[start_id + 1][target_sum - num[start_id - 1]] + dp[start_id + 1][target_sum]    
+            dp[target_sum] = dp[target_sum - num[start_id - 1]] + dp[target_sum]    
           else:
-            dp[start_id][target_sum] = dp[start_id + 1][target_sum]
-    return dp[1][sum1]
-                                             
+            dp[target_sum] = dp[target_sum]
+    return dp[sum1]
+                                                                                          
