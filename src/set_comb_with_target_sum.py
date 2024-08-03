c3ab64cc8da171b33d06b4acb2a1f1f77a996757
pranav <pranav8iisc@gmail.com>
2024-08-03 17:28:49 +0530

    WIP 1/n: add bottom up approach

--- src/set_comb_with_target_sum.py
+++ src/set_comb_with_target_sum.py
@@ -1,27 +1,18 @@
 # You are given a set of positive numbers and a target sum ‘S’. Each number should be assigned either a ‘+’ or ‘-’ sign. We need to find the total ways to assign symbols to make the sum of the numbers equal to the target ‘S’.
 class Solution:
-  def helper(self, start_id, A, abs_sum, num, dp):
-    if start_id <= len(num) and A >= 0  and A < 2 * abs_sum: # valid state
-      if dp[start_id][A] != -1: return
-      elif start_id == len(num):
-        target_sum = A - abs_sum
-        if abs(target_sum) == abs(num[start_id - 1]):
-          dp[start_id][A] = 1
-        else:
-          dp[start_id][A] = 0
-      else:                            
-        dp[start_id][A] = 0 
-        if A - num[start_id - 1] >= 0:
-          self.helper(start_id + 1, A - num[start_id - 1], abs_sum, num, dp) 
-          dp[start_id][A] = dp[start_id + 1][A - num[start_id - 1]]
-        if A + num[start_id - 1] <= 2 * abs_sum:
-          self.helper(start_id + 1, A + num[start_id - 1], abs_sum, num, dp)        
-          dp[start_id][A] += dp[start_id + 1][A + num[start_id - 1]]
   
-  def findTargetSubsets(self, num, s):
-    abs_sum = 0
-    for number in num:
-      abs_sum += abs(number)
-    dp = [[-1 for _ in range(2 * abs_sum + 1)] for _ in range(len(num) + 1)]
-    self.helper(1, s + abs_sum, abs_sum, num, dp)
-    return dp[1][s + abs_sum]
+  def findTargetSubsets(self, nums, target):
+    total_sum = sum([abs(num) for num in nums])
+    dp = [[-1 for _ in range(2 * total_sum + 1)] for  _ in range(len(nums) + 1)]
+    for start_id in range(len(nums), 0, -1):
+      for rem_sum in range(2 * total_sum + 1):
+        if start_id == len(nums):
+          t = rem_sum - total_sum
+          if abs(t) == abs(nums[start_id - 1]):
+            dp[start_id][rem_sum] = 1
+          else:
+            dp[start_id][rem_sum] = 0
+        else:
+          dp[start_id][rem_sum] = dp[start_id + 1][rem_sum - nums[start_id - 1]] + dp[start_id + 1][rem_sum + nums[start_id + 1]]
+    return dp[1][total_sum + target]
+      
